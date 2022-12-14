4b2cf22c107d6da399ca680e4ffc08805e6cca4e
Julian  Zylber <JULIAN.ZYLBER@ort.edu.ar>
2022-12-14 09:55:07 -0300

    feat: Título y barra de navegación

---
+++ components/navbar.tsx
@@ -0,0 +1,12 @@
+export default function NavBar(){
+    return(
+        <nav>
+            <ul>
+                <li>inicio</li>
+                <li>oma</li>
+                <li>ñandú</li>
+                <li>internacional</li>
+            </ul>
+        </nav>
+    )
+}
