0d69f9207080180051dcfd064c7f8548bd84d37b
JZylber <zylberjulian@gmail.com>
2022-12-28 12:23:31 -0300

    feat: Title component

---
+++ components/Title.tsx
@@ -0,0 +1,11 @@
+import styles from './styles/Title.module.css'
+
+export default function Title(){
+    return(
+        <div className={styles.title}>
+            <h1>oma</h1>
+            <h2>San Isidro</h2>
+            <p>Secretaría Regional Buenos Aires Norte de las Olimpiadas de Matemática Argentinas.</p>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Faq from '../components/Faq';
 import NavBar from '../components/Navbar';
 import News from '../components/News';
+import Title from '../components/Title';
 import styles from './styles/Home.module.css'
 
 export default function Home() {
@@ -11,14 +12,13 @@ export default function Home() {
         <title>OMA San Isidro</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Unbounded&display=swap" rel="stylesheet"/> 
     </Head>
     <NavBar></NavBar>
     <main className={styles.wrapper}>
-    <div className={styles.title}>
-      <h1>OMA</h1>
-      <h2>San Isidro</h2>
-      <p>Secretaría regional buenos aires norte</p>
-    </div>
+    <Title></Title>
     <section className={styles.section}>
       <h3>Novedades</h3>
       <News></News>
