72f13a15a60b6add50096052dc336c3ea22a2b3e
JZylber <zylberjulian@gmail.com>
2022-12-29 17:42:35 -0300

    global colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,8 @@ import Faq from '../components/FAQ/Faq';
 import NavBar from '../components/NavBar/Navbar';
 import News from '../components/News/News';
 import Title from '../components/Title/Title';
-import styles from './styles/Home.module.css'
+import styles from './styles/Home.module.scss'
+import MainNandu from '../img/mainÑandu.svg'
 
 const Home: NextPage = () => {
   return (
@@ -13,12 +14,17 @@ const Home: NextPage = () => {
         <title>OMA San Isidro</title>
     </Head>
     <NavBar/>
-    <main className={styles.wrapper}>
-    <Title/>
-    <section className={styles.section}>
-      <h3 className={styles.sectionTitle}>Novedades</h3>
-      <News/>
-    </section>
+    <main>
+    <div className={styles.topSection}>
+      <div className={styles.titleNews}>
+        <Title/>
+        <section>
+          <h3 className={styles.sectionTitle}>Novedades</h3>
+          <News/>
+        </section>
+      </div>
+      <MainNandu className={styles.mainImage}/>
+    </div>
     <section className={styles.section}>
       <h3 className={styles.sectionTitle}>Preguntas Frecuentes</h3>
       <Faq/>
