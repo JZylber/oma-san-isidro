7d12c80d8c96609bfa6565ea7bf5c997303d6037
JZylber <zylberjulian@gmail.com>
2022-12-31 13:38:56 -0300

    feat(index): desktop FAQ

--- components/FAQ/Faq.tsx
+++ components/FAQ/Faq.tsx
@@ -6,5 +6,7 @@ export default function Faq(){
     <div className={styles.container}>
         <FaqItem question='¿OMA o Ñandú?' answer='OMA es para 2°, 3°, 4°, 5°, 6° y 7° (colegios técnicos) secundario. Ñandú es para 5° y 6° primario y 1° secundario'></FaqItem>
         <FaqItem question='¿Y las charito?' answer='Las rondas charito no son parte de la OMA. Nosotros no las organizamos'></FaqItem>
+        <FaqItem question='¿OMA o Ñandú?' answer='OMA es para 2°, 3°, 4°, 5°, 6° y 7° (colegios técnicos) secundario. Ñandú es para 5° y 6° primario y 1° secundario'></FaqItem>
+        <FaqItem question='¿Y las charito?' answer='Las rondas charito no son parte de la OMA. Nosotros no las organizamos'></FaqItem>
     </div>)
 }
