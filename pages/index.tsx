018f84da28aa5888f125096af45b18c98605c0df
AlurrAldrey <48604870+AlurrAldrey@users.noreply.github.com>
2023-01-03 21:38:45 -0300

    fix responsive desktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import NavBar from '../components/NavBar/Navbar';
 import News from '../components/News/News';
 import Title from '../components/Title/Title';
 import styles from './styles/Home.module.scss'
-import MainNandu from '../img/mainÑandu.svg'
+import MainNandu from '../img/mainNandu.svg'
 import Footer from '../components/Footer/Footer';
 import { useState } from 'react';
 
@@ -21,23 +21,25 @@ const Home: NextPage = () => {
     <Head>
         <title>OMA San Isidro</title>
     </Head>
-    <div className={styles.pageLayout}>
-    <NavBar togglePageContent={togglePageContent}/>
-    {showContent && 
-    <main className={styles.main}>
-    <Title/>
-    <section className={styles.newsSection}>
-      <h3 className={styles.sectionTitle}>Novedades</h3>
-      <News/>
-    </section>
-    <MainNandu className={styles.mainImage}/>
-    <section className={styles.FAQSection}>
-      <h3 className={styles.sectionTitle}>Preguntas Frecuentes</h3>
-      <Faq/>
-    </section>
-    <Footer></Footer>
-    </main>
-    }
+    <div className={styles.wrapperLayout}>
+      <div className={styles.pageLayout}>
+        <NavBar togglePageContent={togglePageContent}/>
+        {showContent && 
+        <main className={styles.main}>
+          <Title/>
+          <section className={styles.newsSection}>
+            <h3 className={styles.sectionTitle}>Novedades</h3>
+            <News/>
+          </section>
+          <MainNandu className={styles.mainImage}/>
+          <section className={styles.FAQSection}>
+            <h3 className={styles.sectionTitle}>Preguntas Frecuentes</h3>
+            <Faq/>
+          </section>
+          <Footer></Footer>
+        </main>
+        }
+      </div>
     </div>
     </>
   )
