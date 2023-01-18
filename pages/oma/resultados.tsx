a11716a9340d2d04376d44d1a645517bbdd7ebac
Julián Zylber <48847563+JZylber@users.noreply.github.com>
2023-01-17 23:59:16 -0300

    feat(results): provisional styling

--- pages/oma/resultados.tsx
+++ pages/oma/resultados.tsx
@@ -1,13 +1,16 @@
 import { NextPage } from "next";
 import NavBar from "../../components/NavBar/Navbar";
 import Results from "../../components/ResultsPage/results";
+import styles from "./resultados.module.scss"
 
 const OMAResults : NextPage = () => {
     return(
-        <>
+        <div className={styles.wrapper}> 
+        <main className={styles.main}>
         <NavBar/>
         <Results competition="OMA"/>
-        </>
+        </main>
+        </div>
         )
 }
 
