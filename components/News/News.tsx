8cbf736d3617c68f09dc73989e3ba77a20240458
JZylber <zylberjulian@gmail.com>
2023-02-17 18:50:43 -0300

    feat(news): added links to buttons

--- components/News/News.tsx
+++ components/News/News.tsx
@@ -1,11 +1,12 @@
+import { useRouter } from "next/router";
 import NewsItem from "./NewsItem";
 
 export default function News(){
     return(
     <div>
-        <NewsItem content="Aprobados Nacional Ñandú"></NewsItem>
-        <NewsItem content="Aprobados Nacional OMA"></NewsItem>
-        <NewsItem content="Sedes 2023"></NewsItem>
+        <NewsItem content="Aprobados Nacional Ñandú" link={"/"}></NewsItem>
+        <NewsItem content="Aprobados Nacional OMA" link={"/oma/resultados?año=2022&instancia=NACIONAL"}></NewsItem>
+        <NewsItem content="Sedes 2023" link={"/"}></NewsItem>
     </div>
     )
 }
--- components/News/NewsItem.tsx
+++ components/News/NewsItem.tsx
@@ -1,15 +1,19 @@
 import styles from "./NewsItem.module.scss";
 import NewsArrow from "../../img/newsArrow.svg";
+import Link from "next/link";
 
 interface NewsItemProps {
   content: string;
+  link: string;
 }
 
 export default function NewsItem(props: NewsItemProps) {
   return (
-    <div className={styles.container}>
-      <span>{props.content} </span>
-      <NewsArrow className={styles.arrow} />
-    </div>
+    <Link href={props.link} className={styles.link}>
+      <div className={styles.container}>
+        <span>{props.content} </span>
+        <NewsArrow className={styles.arrow} />
+      </div>
+    </Link>
   );
 }
