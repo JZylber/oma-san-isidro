46997006034597d8e0100d6315140410c5e9b319
JZylber <zylberjulian@gmail.com>
2023-02-27 15:22:10 -0300

    feat(calendar): added clickable month selector

---
+++ components/MonthSelect/MonthSelect.tsx
@@ -0,0 +1,26 @@
+import { Dispatch, SetStateAction } from "react";
+import styles from "./MonthSelect.module.scss";
+import MenuArrow from '../../img/menuArrow.svg';
+
+interface MonthSelectProps {
+    displayedMonth : number,
+    setDisplayedMonth: Dispatch<SetStateAction<number>>;
+}
+
+const MonthSelect = ({displayedMonth,setDisplayedMonth}: MonthSelectProps) => {
+    const months = ["ENE","FEB","MAR","ABR","MAY","JUN","JUL","AGO","SEP","OCT","NOV","DIC"]
+    const monthClick = (forward: boolean) => {
+        setDisplayedMonth(forward? (displayedMonth + 1% months.length): ((displayedMonth - 1) % months.length  + months.length) % months.length)
+    }
+
+    return(
+        <div className={styles.container}>
+            <span onClick={() => monthClick(false)} className={styles.sideMonth}>{months[((displayedMonth - 1) % months.length  + months.length) % months.length]}</span>
+            <MenuArrow onClick={() => monthClick(false)} className={styles.rotated}/>
+            <span className={styles.selectedMonth}>{months[(displayedMonth) % months.length]}</span>
+            <MenuArrow onClick={() => monthClick(true)}/>
+            <span onClick={() => monthClick(true)} className={styles.sideMonth}>{months[(displayedMonth + 1) % months.length]}</span>
+        </div>)
+}
+
+export default MonthSelect
--- pages/calendario.tsx
+++ pages/calendario.tsx
@@ -1,7 +1,10 @@
 import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import Layout from "../components/Layout/Layout";
+import MonthSelect from "../components/MonthSelect/MonthSelect";
 import { getCalendarEvents } from "../lib/aux_db_calls";
+import styles from "./styles/Calendar.module.scss";
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
     const available = await getCalendarEvents(2022)
@@ -19,16 +22,16 @@ interface calendarEvents{
 }
 
 const Calendar : NextPage<{results : Array<calendarEvents>}> = ({results}) => {
+    const [displayedMonth,setDisplayedMonth] = useState(new Date().getMonth())
     return(
         <>
         <Head>
             <title>Calendario</title>
         </Head>
         <Layout>
-            <h1>Calendario</h1>
-            <div>
-                {results.map((event) => <div>{event.fecha_inicio.toString()}</div>)}
-            </div>
+            <h1 className={styles.calendar_title}>Calendario</h1>
+            <hr className={styles.divider}></hr>
+            <MonthSelect displayedMonth={displayedMonth} setDisplayedMonth={setDisplayedMonth}/>
         </Layout>
         </>
         )
