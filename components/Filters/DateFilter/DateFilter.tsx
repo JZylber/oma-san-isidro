f339faa6263fb648d387ee7705cf64bab3825cf4
JZylber <zylberjulian@gmail.com>
2023-02-27 23:56:12 -0300

    feat(calendar): added filter

---
+++ components/Filters/DateFilter/DateFilter.tsx
@@ -0,0 +1,58 @@
+import { Dispatch, RefObject, SetStateAction, useEffect, useRef, useState } from "react";
+import styles from "./DateFilter.module.scss";
+import SelectIcon from "../../../img/menuSelectIcon.svg";
+import FilterIcon from "../../../img/filter.svg";
+
+interface DateFilterProps{
+    availableCategories: string [],
+    categories: string [],
+    setCategories: Dispatch<SetStateAction<string []>>
+}
+
+const DateFilter = ({availableCategories,categories,setCategories}:DateFilterProps) => {
+    const [canOpen,setCanOpen] = useState(false)
+    const toggleFilter = () => {
+        setCanOpen(!canOpen)
+    }
+    const useOutsideAlerter = (ref : RefObject<HTMLDivElement>) => {
+        useEffect(() => {
+          const handleClickOutside = (event : MouseEvent) => {
+            const target = event.target && event.target as Node
+            if (ref.current && !ref.current.contains(target)) {
+              setCanOpen(false);
+            }
+          }
+          document.addEventListener("mousedown", handleClickOutside);
+          return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+          };
+        }, [ref]);
+      }
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    useOutsideAlerter(wrapperRef);
+    const addFilter = (category: string) => {
+        setCategories([...categories,category]);
+        setCanOpen(false);
+    }
+    const clearFilters = () => {
+        setCategories([]);
+    }
+    const isOpen = canOpen && (availableCategories.length - categories.length > 0)
+  return (
+    <>
+    <div className={styles.filter_box} ref={wrapperRef}>
+        <div onClick={toggleFilter} className={[styles.filterText,styles.filterTitle,isOpen?styles.filterTitleOpen:""].join(" ")}><div className={styles.filterTitleStart}><FilterIcon/></div><span>Filtrar</span><div className={styles.filterTitleEnd}><SelectIcon/></div></div>
+        {isOpen && 
+            <ul className={styles.dropdownFilter}>
+                {availableCategories.filter((category) => !categories.includes(category)).map((category,idx) => <li onClick={() => addFilter(category)} className={[styles.filterText,styles.filterOption].join(" ")} key={idx}><span>{category}</span></li>)}
+            </ul>}
+    </div>
+    {categories.length>0 && <div className={styles.currentFilter}>
+        <div onClick={clearFilters} className={[styles.currentFilter_chip,styles.currentFilter_chip_clear].join(" ")}><span>X</span></div>
+        {categories.map((category,idx) => <div className={styles.currentFilter_chip} key={idx}><span key={idx}>{category}</span></div>)}
+    </div>}
+    </>
+  );
+};
+
+export default DateFilter;
--- pages/calendario.tsx
+++ pages/calendario.tsx
@@ -2,14 +2,16 @@ import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
 import DateCard from "../components/DateCard/DateCard";
+import DateFilter from "../components/Filters/DateFilter/DateFilter";
 import Layout from "../components/Layout/Layout";
 import MonthSelect from "../components/MonthSelect/MonthSelect";
 import { getCalendarEvents } from "../lib/aux_db_calls";
 import styles from "./styles/Calendar.module.scss";
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const available = await getCalendarEvents(2022)
-    const newProps = {results: JSON.parse(JSON.stringify(available.results))}
+    const year = 2022
+    const available = await getCalendarEvents(year)
+    const newProps = {results: JSON.parse(JSON.stringify(available.results)),year: year}
     return {
       props: newProps,
     };     
@@ -22,21 +24,27 @@ export interface CalendarEvent{
     texto: string
 }
 
-const Calendar : NextPage<{results : Array<CalendarEvent>}> = ({results}) => {
+const Calendar : NextPage<{results : Array<CalendarEvent>,year:number}> = ({results,year}) => {
     const events = results.map((calendarEvent) => {return ({...calendarEvent,fecha_inicio:new Date(calendarEvent.fecha_inicio), fecha_fin: calendarEvent.fecha_fin?new Date(calendarEvent.fecha_fin):undefined})})
     const [displayedMonth,setDisplayedMonth] = useState(new Date().getMonth())
+    const [categories,setCategories] = useState<string []>([])
     const getMonthEvents = (month: number) => {
-        return(events.filter((event) => event.fecha_inicio.getMonth() === month))
-    } 
+        let monthEvents = events.filter((event) => event.fecha_inicio.getMonth() === month)
+        if(categories.length > 0){
+            monthEvents = monthEvents.filter((event) => categories.includes(event.tipo))
+        }
+        return(monthEvents)
+    }
     return(
         <>
         <Head>
             <title>Calendario</title>
         </Head>
         <Layout>
-            <h1 className={styles.calendar_title}>Calendario</h1>
+            <h1 className={styles.calendar_title}>Calendario {year}</h1>
             <hr className={styles.divider}></hr>
             <MonthSelect displayedMonth={displayedMonth} setDisplayedMonth={setDisplayedMonth}/>
+            <DateFilter categories={categories} setCategories={setCategories} availableCategories={Array.from(new Set(events.map((e) => e.tipo)))}/>
             <div className={styles.events}>
                 {getMonthEvents(displayedMonth).map((event,idx) => <DateCard key={idx} calendarEvent={event}/>)}
             </div>
