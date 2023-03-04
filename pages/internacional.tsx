11d9774486ec6892081a9f290692885ee7e3c9bc
JZylber <zylberjulian@gmail.com>
2023-03-03 22:13:55 -0300

    feat(international): mobile oma version

--- pages/internacional.tsx
+++ pages/internacional.tsx
@@ -2,8 +2,23 @@ import { NextPage } from "next";
 import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import styles from "./styles/International.module.scss";
+import dataOMA from "../data/internacionalOMA.json"
 
 const Internacional : NextPage = () => {
+    const renderOMAparticipant = (omaParticipant : Array<string | boolean>,idx:number) => {
+        const [level,surname,name,t1,t2,t3,t4] = omaParticipant;
+        return(
+            <tr key={idx}>
+                <td>{level}</td>
+                <td>{surname}</td>
+                <td>{name}</td>
+                <td>{t1?"Si":""}</td>
+                <td>{t2?"Si":""}</td>
+                <td>{t3?"Si":""}</td>
+                <td>{t4?"Si":""}</td>
+            </tr>)
+    }
+
     return(
         <>
         <Head>
@@ -31,7 +46,44 @@ const Internacional : NextPage = () => {
                          <h3 className={styles.tournament_name}>Selección IMO</h3>
                          <p className={styles.tournament_requirements}>Haber aprobado la instancia Nacional de OMA, haber nacido después del 01/01/2003 y ser alumno regular de enseñanza media en el 2023.</p>
                     </li>
+                    <li className={styles.tournament}>
+                         <h3 className={styles.tournament_name}>Selección Iberoamericana</h3>
+                         <p className={styles.tournament_requirements}>Haber aprobado la instancia Nacional de OMA, haber nacido después del 01/01/2005 y no haber participado en dos Olimpíadas Iberoamericanas anteriores.</p>
+                    </li>
+                    <li className={styles.tournament}>
+                         <h3 className={styles.tournament_name}>Olimpíada Iraní de Geometría</h3>
+                         <p className={styles.tournament_requirements}>Alumnos de OMA y Ñandú seleccionados por el Comité Olímpico.</p>
+                    </li>
+                    <li className={styles.tournament}>
+                         <h3 className={styles.tournament_name}>Olimpíada Iraní de Combinatoria</h3>
+                         <p className={styles.tournament_requirements}>Alumnos de OMA y Ñandú seleccionados por el Comité Olímpico.</p>
+                    </li>
+                    <li className={styles.tournament}>
+                         <h3 className={styles.tournament_name}>Olimpíada Panamericana Femenil de Matemática</h3>
+                         <p className={styles.tournament_requirements}>Alumnas de OMA seleccionadas por el Comité Olímpico.</p>
+                    </li>
+                    <li className={styles.tournament}>
+                         <h3 className={styles.tournament_name}>OlimPri</h3>
+                         <p className={styles.tournament_requirements}>Campeones y subcampeones de 1° y 2° Nivel de Ñandú 2022.</p>
+                    </li>
                 </ul>
+                <table className={styles.table}>
+                    <thead className={styles.table_header}>
+                        <tr>
+                            <th className={styles.table_column_level}>Nivel</th>
+                            <th className={styles.table_column_name}>Apellido</th>
+                            <th className={styles.table_column_name}>Nombre</th>
+                            <th className={styles.table_column_tournament}>Mayo</th>
+                            <th className={styles.table_column_tournament}>Cono Sur</th>
+                            <th className={styles.table_column_tournament}>IMO</th>
+                            <th className={styles.table_column_tournament}>Ibero</th>
+                        </tr>
+                    </thead>
+                    <tbody className={styles.table_body}>
+                        {dataOMA.map(renderOMAparticipant)}
+                    </tbody>
+                </table>
+
             </div>
             <div className={styles.category}>
                 <h2 className={styles.category_title}>Alumnos que participaron en Ñandú en 2022</h2>
