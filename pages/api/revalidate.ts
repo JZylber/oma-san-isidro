1c463ccfd5cf96aec5872e8e7ab7784f11bfa45c
JZylber <zylberjulian@gmail.com>
2023-03-10 22:18:52 -0300

    fix(revalidation): path for calendar revalidation is now correct

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -22,7 +22,7 @@ export default async function handler(req : NextApiRequest, res:NextApiResponse)
       }
     }else if(req.query.type === "calendar"){
       try {
-        await res.revalidate('/calendar')
+        await res.revalidate('/calendario')
         return res.json({ revalidated: true })
       } catch (err) {
         return res.status(500).send('Error revalidating')
