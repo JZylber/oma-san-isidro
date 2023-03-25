3bec96b6c413c11f2dbc0b3373bcc92c8a82642c
JZylber <zylberjulian@gmail.com>
2023-03-25 17:33:03 -0300

    feat(geometria): added inscription information

--- components/Inscription/BankInformation.tsx
+++ components/Inscription/BankInformation.tsx
@@ -6,9 +6,9 @@ const BankInformation = () => {
         <div className={styles.border_box}>
             <h3>El depósito debe realizarse a nombre de: Fundación Olimpíada Matemática Argentina en cualquier sucursal del banco HSBC (Cuenta Corriente)</h3>
             <ul className={styles.bank_data}>
-                <li>N° Cuenta: 6093228419</li>
-                <li>CBU: 15006099 - 00060932284196</li>
-                <li>CUIT: 30-67928383-5</li>
+                <li className={styles.item}>N° Cuenta: 6093228419</li>
+                <li className={styles.item}>CBU: 15006099 - 00060932284196</li>
+                <li className={styles.item}>CUIT: 30-67928383-5</li>
             </ul>
         </div>
     </div>       
