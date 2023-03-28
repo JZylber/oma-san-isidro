ed176623a0fae1d228d7deb349faa70cf8050dc9
JZylber <zylberjulian@gmail.com>
2023-03-28 18:19:14 -0300

    feat(calendar): calendar markdown generation and printing

---
+++ components/CalendarComponents/MarkdownCalendar/MarkdownCalendar.tsx
@@ -0,0 +1,26 @@
+import { ReactMarkdown } from "react-markdown/lib/react-markdown"
+import {CalendarEvent} from "../CalendarTypes";
+
+interface MarkdownCalendarProps{
+    year: number,
+    events: {name: string, events: CalendarEvent []}[]
+}
+
+const MarkdownCalendar = ({year,events}: MarkdownCalendarProps) => {
+    let markdown = `# Calendario ${year}`
+    events.forEach((month) => {
+        if(month.events.length > 0){
+            let newMonth = `\n## ${month.name}`
+            month.events.forEach((event) => {
+                let newEvent = `\n- *${event.fecha_inicio.getDate()}${event.fecha_fin?"-"+event.fecha_fin.getDate():""}* **(${event.tipo})** ${event.texto}`;
+                newMonth += newEvent;
+            })
+            markdown += newMonth;
+        }
+    })
+    return(
+        <ReactMarkdown children={markdown}/>
+    )
+}
+
+export default MarkdownCalendar
