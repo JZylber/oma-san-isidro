b494234d6645057ab18ccabd306d436dd080d906
JZylber <zylberjulian@gmail.com>
2023-03-28 23:40:55 -0300

    feat(download results): result options

--- components/ResultsPage/DownloadModal.tsx
+++ components/ResultsPage/DownloadModal.tsx
@@ -5,6 +5,7 @@ import X from "../../public/images/x.svg";
 import CSV from "../../public/images/csv.svg";
 import XLS from "../../public/images/xls.svg";
 import PDF from "../../public/images/pdf.svg";
+import { Button } from "../buttons/Button";
 
 interface DownloadModalProps{
     open: boolean,
@@ -13,6 +14,7 @@ interface DownloadModalProps{
 
 const DownloadPopup = ({open,setOpen}: DownloadModalProps) => {
     const [format,setFormat] = useState("csv");
+    const [allResults,setAllResults] = useState(false);
     return(
         <Modal open={open}>
             <div className={styles.container}>
@@ -24,26 +26,45 @@ const DownloadPopup = ({open,setOpen}: DownloadModalProps) => {
                 <h1 className={styles.title}>Descarga</h1>
                 <h2 className={styles.option_title}>Formato</h2>
                     <div className={styles.format_options}>
-                        <div className={styles.format_option}>
+                        <div className={styles.option}>
                             <div className={[styles.format_container,format==="csv" && styles.selected].join(" ")} onClick={() => setFormat("csv")}>
                                 <CSV/>
                             </div>
-                            <p>CSV</p>
+                            <p className={styles.format_text}>CSV</p>
                         </div>
-                        <div className={styles.format_option}>
+                        <div className={styles.option}>
                             <div className={[styles.format_container,format==="xls" && styles.selected].join(" ")} onClick={() => setFormat("xls")}>
                                 <XLS/>
                             </div>
-                            <p>XLS</p>
+                            <p className={styles.format_text}>XLS</p>
                         </div>
-                        <div className={styles.format_option}>
+                        <div className={styles.option}>
                             <div className={[styles.format_container,format==="pdf" && styles.selected].join(" ")} onClick={() => setFormat("pdf")}>
                                 <PDF/>
                             </div>
-                            <p>PDF</p>
+                            <p className={styles.format_text}>PDF</p>
                         </div>
                     </div>
                 <h2 className={styles.option_title}>Resultados</h2>
+                    <div className={styles.result_options}>
+                        <div className={[styles.option,styles.minWidth].join(" ")}>
+                            <div className={[styles.result_button,!allResults && styles.selected].join(" ")} onClick={() => setAllResults(false)}>
+                                Obtenidos
+                            </div>
+                            <p className={styles.result_text}>Los resultados obtenidos por los filtros aplicados.</p>
+                        </div>
+                        <div className={[styles.option,styles.minWidth].join(" ")}>
+                            <div className={[styles.result_button,allResults && styles.selected].join(" ")} onClick={() => setAllResults(true)}>
+                                Todos
+                            </div>
+                            <p className={styles.result_text}>Todos los resultados de <span className={styles.result_text_bold}>2023 zonal</span>.</p>
+                        </div>
+                    </div>
+                <div className={styles.button_container}>
+                    <Button content="Confirmar Descarga">
+                        <></>
+                    </Button>
+                </div>
             </div>
         </Modal>
     )
