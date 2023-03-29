a4a635eec2482adb1faff510499a0e2acc6243dd
JZylber <zylberjulian@gmail.com>
2023-03-28 23:23:48 -0300

    feat(download results): format options

--- components/ResultsPage/DownloadModal.tsx
+++ components/ResultsPage/DownloadModal.tsx
@@ -1,7 +1,10 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import Modal from "../Popups/Modal";
 import styles from "./DownloadModal.module.scss";
 import X from "../../public/images/x.svg";
+import CSV from "../../public/images/csv.svg";
+import XLS from "../../public/images/xls.svg";
+import PDF from "../../public/images/pdf.svg";
 
 interface DownloadModalProps{
     open: boolean,
@@ -9,6 +12,7 @@ interface DownloadModalProps{
 }
 
 const DownloadPopup = ({open,setOpen}: DownloadModalProps) => {
+    const [format,setFormat] = useState("csv");
     return(
         <Modal open={open}>
             <div className={styles.container}>
@@ -18,6 +22,28 @@ const DownloadPopup = ({open,setOpen}: DownloadModalProps) => {
                     </div>
                 </div>
                 <h1 className={styles.title}>Descarga</h1>
+                <h2 className={styles.option_title}>Formato</h2>
+                    <div className={styles.format_options}>
+                        <div className={styles.format_option}>
+                            <div className={[styles.format_container,format==="csv" && styles.selected].join(" ")} onClick={() => setFormat("csv")}>
+                                <CSV/>
+                            </div>
+                            <p>CSV</p>
+                        </div>
+                        <div className={styles.format_option}>
+                            <div className={[styles.format_container,format==="xls" && styles.selected].join(" ")} onClick={() => setFormat("xls")}>
+                                <XLS/>
+                            </div>
+                            <p>XLS</p>
+                        </div>
+                        <div className={styles.format_option}>
+                            <div className={[styles.format_container,format==="pdf" && styles.selected].join(" ")} onClick={() => setFormat("pdf")}>
+                                <PDF/>
+                            </div>
+                            <p>PDF</p>
+                        </div>
+                    </div>
+                <h2 className={styles.option_title}>Resultados</h2>
             </div>
         </Modal>
     )
