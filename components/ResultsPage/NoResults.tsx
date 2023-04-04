01f31c2849c6b209558356b8f0bb21a652d802f4
JZylber <zylberjulian@gmail.com>
2023-04-04 12:52:50 -0300

    feat(no results): mobile typography

--- components/ResultsPage/NoResults.tsx
+++ components/ResultsPage/NoResults.tsx
@@ -9,8 +9,10 @@ const NoResults = () => {
             </div>
             <div className={styles.message}>
                 <p className={styles.message_title}>La consulta no arrojó ningún resultado :(</p>
-                <p className={styles.message_body}>No hay resultados para los filtros seleccionados</p>
-                <p className={styles.message_body}>Cambie los filtros e intente nuevamente</p>
+                <span className={styles.message_body_container}>
+                    <span className={styles.message_body}>No hay resultados para los filtros seleccionados.</span>
+                    <span className={styles.message_body}>Cambie los filtros e intente nuevamente.</span>
+                </span>
             </div>
         </div>
     )
