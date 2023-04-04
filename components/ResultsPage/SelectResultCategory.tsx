3b38ff0a9d3545d310df66b048886b2de7f5a5ac
JZylber <zylberjulian@gmail.com>
2023-04-04 16:52:55 -0300

    feat(mobile results): filter buttons

--- components/ResultsPage/SelectResultCategory.tsx
+++ components/ResultsPage/SelectResultCategory.tsx
@@ -1,4 +1,4 @@
-import {RefObject, useCallback, useEffect, useRef, useState } from "react"
+import {RefObject, useCallback, useEffect, useMemo, useRef, useState } from "react"
 import SelectIcon from "../../public/images/menuSelectIcon.svg";
 import { School } from "./resultsTypes";
 import styles from "./SelectResultCategory.module.scss"
@@ -8,12 +8,20 @@ interface SelectResultProps<T>{
     value: T | undefined,
     setValue: (option: T | undefined) => void,
     options: T [],
+    allOptions?: T[],
     input?: boolean,
     clear?: boolean,
     sortOptions?: (option_a : T,option_b: T) => number;
+    buttons?: boolean;
 }
 
-const SelectResultCategory = <T extends string|number|boolean|School,>({category,value,setValue,options,input = false,clear=false,sortOptions}:SelectResultProps<T>) => {
+const SelectResultCategory = <T extends string|number|boolean|School,>({category,value,setValue,options,allOptions,input = false,clear=false,buttons=false,sortOptions}:SelectResultProps<T>) => {
+    if(buttons && (input || clear)){
+      throw Error("SelectResultCategory: Buttons can't be used with input or clear options");
+    }
+    if(allOptions && !buttons){
+      throw Error("SelectResultCategory: AllOptions can only be used with buttons");
+    }
     sortOptions && options.sort(sortOptions);
     const [canOpen,setCanOpen] = useState(false)
     const toggleFilter = () => {
@@ -69,39 +77,56 @@ const SelectResultCategory = <T extends string|number|boolean|School,>({category
     const wrapperRef = useRef<HTMLDivElement>(null);
     useOutsideAlerter(wrapperRef);
     const isOpen = canOpen && options.length > 0
-  return (
-    <div className={styles.category_container}>
-      <p className={styles.category}>{category}</p>
-      <div className={styles.filter_box_container}>
-        <div className={[styles.filter_box,isOpen && styles.filter_box_open].join(" ")} ref={wrapperRef}>
-          {!input?
-            <div onClick={toggleFilter} className={[styles.filterText,styles.filterTitle,isOpen?styles.filterTitleOpen:""].join(" ")}>
-              <span>{value!==undefined?displayOption(value):`-`}</span>
-              <div className={styles.filterTitleEnd}>
-                <SelectIcon/>
+    const toggleValue = (option : T) => {
+      if(value === option){
+        setValue(undefined);
+      }else{
+        setValue(option);
+      }
+      toggleFilter();
+    }
+    const optionsToDisplay = allOptions?allOptions:options;
+    const [initialOptions] = useState(options);
+    return (
+      <div className={styles.category_container}>
+        <p className={styles.category}>{category}</p>
+        {!buttons && <div className={styles.filter_box_container}>
+          <div className={[styles.filter_box,isOpen && styles.filter_box_open].join(" ")} ref={wrapperRef}>
+            {!input?
+              <div onClick={toggleFilter} className={[styles.filterText,styles.filterTitle,isOpen?styles.filterTitleOpen:""].join(" ")}>
+                <span>{value!==undefined?displayOption(value):`-`}</span>
+                <div className={styles.filterTitleEnd}>
+                  <SelectIcon/>
+                </div>
               </div>
-            </div>
-            :
-            <div className={[styles.filter_input,isOpen?styles.filterTitleOpen:""].join(" ")}>
-            <input 
-              className={[styles.clean_input,styles.filterText,styles.filterTitle].join(" ")} 
-              value={tempValue}
-              onChange={(event) => {setTempValue(event.target.value);setCanOpen(true)}}
-            />
-            {value&&<div className={[styles.filterText,styles.filter_clear].join(" ")} onClick={cleanFilter}>X</div>}
-            </div>
-            }
-          {isOpen && 
-              <ul className={styles.dropdownFilter}>
-                  {clear && <li onClick={() => {setValue(undefined);toggleFilter()}} className={[styles.filterText,styles.filterOption].join(" ")}><span>Quitar filtro</span></li>}
-                  {input?
-                    filteredOptions.map((option,idx) => <li onClick={() => {setInputFilter(option)}} className={[styles.filterText,styles.filterOption].join(" ")} key={idx}><span>{displayOption(option)}</span></li>)
-                    :options.map((option,idx) => <li onClick={() => {setValue(option);toggleFilter()}} className={[styles.filterText,styles.filterOption].join(" ")} key={idx}><span>{displayOption(option)}</span></li>)}
-              </ul>}
-        </div>
+              :
+              <div className={[styles.filter_input,isOpen?styles.filterTitleOpen:""].join(" ")}>
+              <input 
+                className={[styles.clean_input,styles.filterText,styles.filterTitle].join(" ")} 
+                value={tempValue}
+                onChange={(event) => {setTempValue(event.target.value);setCanOpen(true)}}
+              />
+              {value&&<div className={[styles.filterText,styles.filter_clear].join(" ")} onClick={cleanFilter}>X</div>}
+              </div>
+              }
+            {isOpen &&
+                <ul className={styles.dropdownFilter}>
+                    {clear && <li onClick={() => {setValue(undefined);toggleFilter()}} className={[styles.filterText,styles.filterOption].join(" ")}><span>Quitar filtro</span></li>}
+                    {input?
+                      filteredOptions.map((option,idx) => <li onClick={() => {setInputFilter(option)}} className={[styles.filterText,styles.filterOption].join(" ")} key={idx}><span>{displayOption(option)}</span></li>)
+                      :options.map((option,idx) => <li onClick={() => {setValue(option);toggleFilter()}} className={[styles.filterText,styles.filterOption].join(" ")} key={idx}><span>{displayOption(option)}</span></li>)}
+                </ul>}
+          </div>
+        </div>}
+        {buttons &&
+              <div className={styles.buttons_container}>
+                {
+                optionsToDisplay.map((option,idx) => <div onClick={() => toggleValue(option)} className={[styles.button,value === option?styles.selected:"",(initialOptions.includes(option))?"":styles.unavailable].join(" ")} key={idx}><span>{displayOption(option)}</span></div>)
+                }
+              </div>
+        }
       </div>
-    </div>
-  );
+    );
 }
 
 export default SelectResultCategory
--- components/ResultsPage/resultFilterForm.tsx
+++ components/ResultsPage/resultFilterForm.tsx
@@ -22,7 +22,7 @@ const ResultFilterForm = ({filters,updateFilter,schools,names,levels,passed} : R
         if(!isMobile && openFilters){
             setIsMobile(true);
         }
-    },[openFilters]);
+    },[openFilters,isMobile]);
     return(
         <div className={styles.container}>
             <div className={styles.mobile_filters} onClick={() => setOpenFilters(true)}>
@@ -43,6 +43,8 @@ const ResultFilterForm = ({filters,updateFilter,schools,names,levels,passed} : R
                     <p className={styles.title}>Más filtros</p>
                     <SelectResultCategory category="Participante" value={filters.participante} setValue={(value?: string) => updateFilter("participante",value)} options={names} input={true}/>
                     <SelectResultCategory category="Colegio" value={filters.colegio} setValue={(value? : School) => updateFilter("colegio",value)} options={schools} input={true}/>
+                    <SelectResultCategory category="Nivel" value={filters.nivel} setValue={(value? : number) => updateFilter("nivel",value)} options={levels} allOptions={[1,2,3]} buttons={true}/>
+                <SelectResultCategory category="Aprobado" value={filters.aprobado} setValue={(value? : boolean) => updateFilter("aprobado",value)} options={passed} allOptions={[true,false]} buttons={true}/>
                 </form>
             </Modal>}
         </div>
