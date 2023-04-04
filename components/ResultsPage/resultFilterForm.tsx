0d0a2f53efb84ae4892380a02c8868ca778aaf74
Julián Zylber <julian.zylber@ort.edu.ar>
2023-04-04 11:23:28 -0300

    feat(mobile filters modal)

--- components/ResultsPage/resultFilterForm.tsx
+++ components/ResultsPage/resultFilterForm.tsx
@@ -1,6 +1,10 @@
 import styles from "./resultFilterForm.module.scss"
 import { ResultFilter, School } from "./resultsTypes"
 import SelectResultCategory from "./SelectResultCategory"
+import FilterIcon from "../../public/images/filter.svg"
+import X from "../../public/images/x.svg"
+import Modal from "../Popups/Modal"
+import { useState } from "react"
 
 interface ResultFilterProps{
     filters: ResultFilter,
@@ -12,13 +16,30 @@ interface ResultFilterProps{
 }
 
 const ResultFilterForm = ({filters,updateFilter,schools,names,levels,passed} : ResultFilterProps) => {
+    const [openFilters,setOpenFilters] = useState(false);
     return(
-        <form className={styles.filters}>
-            <SelectResultCategory category="Participante" value={filters.participante} setValue={(value?: string) => updateFilter("participante",value)} options={names} input={true}/>
-            <SelectResultCategory category="Colegio" value={filters.colegio} setValue={(value? : School) => updateFilter("colegio",value)} options={schools} input={true}/>
-            <SelectResultCategory category="Nivel" value={filters.nivel} setValue={(value? : number) => updateFilter("nivel",value)} options={levels} clear={true}/>
-            <SelectResultCategory category="Aprobado" value={filters.aprobado} setValue={(value? : boolean) => updateFilter("aprobado",value)} options={passed} clear={true}/>
-        </form>
+        <div className={styles.container}>
+            <div className={styles.mobile_filters} onClick={() => setOpenFilters(true)}>
+                <FilterIcon/>
+                <span>Más filtros</span>
+            </div>
+            <form className={styles.filters}>
+                <SelectResultCategory category="Participante" value={filters.participante} setValue={(value?: string) => updateFilter("participante",value)} options={names} input={true}/>
+                <SelectResultCategory category="Colegio" value={filters.colegio} setValue={(value? : School) => updateFilter("colegio",value)} options={schools} input={true}/>
+                <SelectResultCategory category="Nivel" value={filters.nivel} setValue={(value? : number) => updateFilter("nivel",value)} options={levels} clear={true}/>
+                <SelectResultCategory category="Aprobado" value={filters.aprobado} setValue={(value? : boolean) => updateFilter("aprobado",value)} options={passed} clear={true}/>
+            </form>
+            <Modal open={openFilters}>
+                <div className={styles.filter_modal}>
+                    <div className={styles.close}>
+                        <X className={styles.icon} onClick={() => setOpenFilters(false)}/>
+                    </div>
+                    <p className={styles.title}>Más filtros</p>
+                    <SelectResultCategory category="Participante" value={filters.participante} setValue={(value?: string) => updateFilter("participante",value)} options={names} input={true}/>
+                    <SelectResultCategory category="Colegio" value={filters.colegio} setValue={(value? : School) => updateFilter("colegio",value)} options={schools} input={true}/>
+                </div>
+            </Modal>
+        </div>
     )
 }
 
