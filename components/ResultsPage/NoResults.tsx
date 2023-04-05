fc0dc518f578f1a3cbe971a242dbea22346acc70
JZylber <zylberjulian@gmail.com>
2023-04-04 22:14:03 -0300

    feat(export results): tablet styling

--- components/ResultsPage/NoResults.tsx
+++ components/ResultsPage/NoResults.tsx
@@ -8,7 +8,7 @@ const NoResults = () => {
                 <Warning/>
             </div>
             <div className={styles.message}>
-                <p className={styles.message_title}>La consulta no arrojó ningún resultado :(</p>
+                <p className={styles.message_title}>La consulta no arrojó ningún resultado</p>
                 <span className={styles.message_body_container}>
                     <span className={styles.message_body}>No hay resultados para los filtros seleccionados.</span>
                     <span className={styles.message_body}>Cambie los filtros e intente nuevamente.</span>
