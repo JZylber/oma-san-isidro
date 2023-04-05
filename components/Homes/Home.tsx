ec9dc386b2df48a7fb50c0c4a7cea0306b561611
JZylber <zylberjulian@gmail.com>
2023-04-05 13:16:07 -0300

    feat(homes): basic structure

--- components/Homes/Home.tsx
+++ components/Homes/Home.tsx
@@ -13,6 +13,13 @@ const Home = ({competition,dates}:HomeProps) => {
         <h1 className={styles.title}>{competition}</h1>
         <h2 className={styles.sectionTitle}>Fechas</h2>
         <DateBanner dates={dates} displayAmount={dates.length} displayCategory={competition} ignoreCurrentDate={true}/>
+        <h2 className={styles.sectionTitle}>Próxima instancia</h2>
+        <p>Aca iría la información de la próxima instancia, en particular, horario y sedes asignadas a cada colegio (es una tabla que indica zona, colegio, y sede, con dirección de la sede). Podría haber un filtro para zona/colegio/sede, pero debería ser una tabla chica, no se si hacen falta</p>
+        <h2 className={styles.sectionTitle}>Niveles</h2>
+        <p>Acá irían la equivalencia entre nivel y año escolar</p>
+        <h2 className={styles.sectionTitle}>Instancias</h2>
+        <p>Acá se mostrarían la progresión de las instancias, preferentemente de forma gráfica. Adjunto una versión ejemplo</p>
+        <img src="/images/instancias.png" alt="Instancia"/>
         </>
     )
 }
