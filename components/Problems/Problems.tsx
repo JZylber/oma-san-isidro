a42c765eca7e5a2bd300b0b543944b1353793690
Julián Zylber <julian.zylber@ort.edu.ar>
2023-04-09 19:52:20 -0300

    feat(problems): added links

--- components/Problems/Problems.tsx
+++ components/Problems/Problems.tsx
@@ -1,9 +1,25 @@
 import styles from "./Problems.module.scss";
+import { Fragment } from "react";
 
-const Problems : ({type}: {type: string}) => JSX.Element = ({type}) => {
+const Problems : ({type}: {type: string}) => JSX.Element = ({type}) => {  
+    const problems : {[key: string]: {[key:string]: string}}= require(`../../data/Problemas${type}.json`);
     return(
     <>
         <h1 className={styles.title}>Problemas</h1>
+        {Object.keys(problems).map((key:string,idx:number) => {
+            const problem = problems[key];
+            return(
+                <Fragment key={idx}>
+                    <h2>{key}</h2>
+                    {Object.keys(problem).map((key:string,idx:number) => {
+                        const problemLink = problem[key];
+                        return(
+                            <a href={problemLink} target="_blank" rel="noopener noreferrer" key={idx}>{key}</a>
+                        )
+                    })}
+                </Fragment>
+            );
+        })}
     </>
     )
 }
