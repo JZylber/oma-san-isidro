57cb03b21b887d02fe53a4146cdc25f4588fc0ba
JZylber <zylberjulian@gmail.com>
2023-04-10 13:05:24 -0300

    feat(venues): added nandu drop point info

--- components/Venues/Venues.tsx
+++ components/Venues/Venues.tsx
@@ -8,6 +8,13 @@ interface VenueInfo {
     auth_max_date?: Date;
 }
 
+interface DropPointInfo {
+    localidad: string,
+    nombre?: string,
+    direccion: string,
+    aclaraciones?: string,
+}
+
 const venueInfo : {[key: string]: VenueInfo} = {
     nandu: {dropPoints: true, venues: false, next_competition: "Interescolar", auth_max_date: new Date(2023, 3, 28)},
     oma: {dropPoints: false, venues: false, next_competition: "Intercolegial"}
@@ -15,16 +22,27 @@ const venueInfo : {[key: string]: VenueInfo} = {
 
 const Venues = ({type}:{type:string}) => {
     const {dropPoints, venues, next_competition,auth_max_date} = venueInfo[type];
-    const dropPointsData = dropPoints ? require(`./data/${type}${next_competition.toLocaleLowerCase()}auth.json`) : null;
+    const dropPointsData : DropPointInfo [] | null = dropPoints ? require(`./data/${type}${next_competition.toLocaleLowerCase()}auth.json`) : null;
     const venuesData = venues ? require(`./data/${type}${next_competition.toLocaleLowerCase()}venues.json`) : null;
     return(
         <>
             <h1 className={styles.title}>Sedes {next_competition}</h1>
             <h2 className={styles.section_title}>Autorizaciones</h2>
-            {dropPointsData ? 
-                <p className={styles.text}>Las autorizaciones se pueden consegui <Link href={`/${type}/autorizacion`}>aquí</Link>. Estas se pueden entregar hasta el {auth_max_date?`${auth_max_date.getDate()}/${auth_max_date.getMonth() + 1}`:"(A definir)"} en los siguientes puntos:</p>
-            : <p>Proximamente...</p>}
+            {dropPointsData ?
+                <>
+                <p className={styles.text}>Las autorizaciones se pueden conseguir <Link href={`/${type}/autorizacion`}>aquí</Link>. Estas se pueden entregar hasta el <span className={styles.bold}>{auth_max_date?`${auth_max_date.getDate()}/${auth_max_date.getMonth() + 1}`:"(A definir)"}</span> en los siguientes puntos:</p>
+                <ul className={styles.dropPoints}>
+                    {dropPointsData.map((dropPoint, index) => {
+                        const {localidad, nombre, direccion, aclaraciones} = dropPoint;
+                        return(<li className={styles.text}><span className={styles.bold}>{localidad}: </span>{nombre?`${nombre} - `:""}{direccion}{aclaraciones?` (${aclaraciones})`:""}</li>)
+                    })}
+                </ul>
+                </>
+            : <p className={styles.text}>Proximamente...</p>}
             <h2 className={styles.section_title}>Sedes</h2>
+            {venuesData ? 
+                <p className={styles.text}></p>
+            : <p className={styles.text}>Proximamente...</p>}
         </>
     )
 }
