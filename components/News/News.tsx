fae03c82d9cc2f4872f81b35b8ad5ec8b9001e45
JZylber <zylberjulian@gmail.com>
2023-05-21 11:46:22 -0300

    feat(preview): checking environment variables

--- components/News/News.tsx
+++ components/News/News.tsx
@@ -4,7 +4,7 @@ import { NewsItemData } from "./NewsTypes";
 export default function News({newsData}:{newsData:NewsItemData[]}){
     const env = process.env.NODE_ENV;
     const vercel_env = process.env.VERCEL_ENV;
-    console.log(env,vercel_env);
+    console.log(env,vercel_env,process.env.VERCEL);
     let newsToDisplay = newsData;
     if(env === "production" && (vercel_env?vercel_env === "production":true)){
         newsToDisplay = newsToDisplay.filter((newsItemData) => newsItemData.visible);
