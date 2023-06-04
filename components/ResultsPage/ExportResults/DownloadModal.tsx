ea8dddf1955454cd5b11ce4e55b84757c7c59c2c
Julián Zylber <julian.zylber@ort.edu.ar>
2023-06-04 19:52:31 -0300

    feat(results): disabled pdf export

--- components/ResultsPage/ExportResults/DownloadModal.tsx
+++ components/ResultsPage/ExportResults/DownloadModal.tsx
@@ -64,7 +64,7 @@ const DownloadPopup = ({open,setOpen,testInfo,results,filteredResults}: Download
                             </div>
                             <p className={styles.format_text}>XLSX</p>
                         </div>
-                        <div className={styles.option}>
+                        <div className={[styles.option,styles.unavailable].join(" ")}>
                             <div className={[styles.format_container,format==="pdf" && styles.selected,styles.grayed].join(" ")} onClick={() => setFormat("pdf")}>
                                 <PDF/>
                             </div>
