c705dae097b4f7ab9239479ce3aa1826f60fb9e0
JZylber <zylberjulian@gmail.com>
2023-06-18 21:11:36 -0300

    refactor(venues): data obtainer function

--- lib/aux_functions.ts
+++ lib/aux_functions.ts
@@ -1,6 +1,24 @@
+import { getInstanceDropPoints, getInstanceVenues, getInstances } from "./aux_db_calls";
+
 export const getDateFromJSON = (JSONDate : string) => {
     let date = JSONDate.split('T')[0];
     let [year,month,day] = date.split('-').map((n) => Number(n));
     month = month - 1;
     return(new Date(year,month,day))
+}
+
+export const venueDataGenerator = async (competition: string, instance_hierarchy: string[]) => {
+    const date= new Date();
+    const year = date.getFullYear();
+    const newProps = await getInstances(competition,year).then(async (instances) => {
+        const {instancia,fecha_limite_autorizacion} = instances.results.filter(instance => instance.fecha > date)[0];
+        const next_instance = instancia;
+        const auth_max_date = fecha_limite_autorizacion;
+        const dropPoints = await getInstanceDropPoints(competition,year,next_instance);
+        const venues = await getInstanceVenues(competition,year,next_instance);
+        return({next_instance: next_instance,venues: venues,dropPoints: dropPoints.results,auth_max_date: JSON.parse(JSON.stringify(auth_max_date))})
+    });
+    return {
+        props: newProps,
+    };
 }
--- pages/nandu/sedes.tsx
+++ pages/nandu/sedes.tsx
@@ -2,28 +2,14 @@ import { GetStaticProps, NextPage } from "next"
 import Head from "next/head"
 import Layout from "../../components/Layout/Layout"
 import Venues, { DropPoint, Venue } from "../../components/Venues/Venues"
-import { getInstanceVenues, getInstanceDropPoints, getInstances } from "../../lib/aux_db_calls";
-import { getDateFromJSON } from "../../lib/aux_functions";
+import { getDateFromJSON, venueDataGenerator } from "../../lib/aux_functions";
+import { School } from "../../components/ResultsPage/resultsTypes";
 
 const competition = "ÑANDÚ";
+const instance_hierarchy = ["INTERESCOLAR","ZONAL","REGIONAL","NACIONAL"];
 
-export const getStaticProps: GetStaticProps= async ({ params }) => {
-    const date= new Date();
-    const year = date.getFullYear();
-    const newProps = await getInstances(competition,year).then(async (instances) => {
-      const {instancia,fecha_limite_autorizacion} = instances.results.filter(instance => instance.fecha > date)[0];
-      const next_instance = instancia;
-      const auth_max_date = fecha_limite_autorizacion;
-      const dropPoints = await getInstanceDropPoints(competition,year,next_instance);
-      const venues = await getInstanceVenues(competition,year,next_instance);
-      return({next_instance: next_instance,venues: venues.results,dropPoints: dropPoints.results,auth_max_date: JSON.parse(JSON.stringify(auth_max_date))})
-    });
-    return {
-      props: newProps,
-    }; 
-  };
 
-const NanduVenues : NextPage<{next_instance: string,venues: Venue[],dropPoints: DropPoint [], auth_max_date: string}> = ({next_instance,venues,dropPoints,auth_max_date}) => {
+const NanduVenues : NextPage<{next_instance: string,venues: Venue<School>[],dropPoints: DropPoint [], auth_max_date: string}> = ({next_instance,venues,dropPoints,auth_max_date}) => {
     const date = auth_max_date?getDateFromJSON(auth_max_date):undefined;
     return(
         <>
