ea0ce737f73e38480f4fcfab6e83013aad70fe88
JZylber <zylberjulian@gmail.com>
2023-07-05 23:44:06 -0300

    feat(instances): query parameters

--- components/Instances/InstanceMenu.tsx
+++ components/Instances/InstanceMenu.tsx
@@ -1,6 +1,7 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import styles from "./InstanceMenu.module.scss";
 import InstanceData from "./Instance";
+import { useRouter } from "next/router";
 
 export interface Instance {
     instancia: string,
@@ -12,13 +13,34 @@ interface InstanceMenuProps {
     instances: Instance[]
 }
 
-const Instances = ({competition,instances}: InstanceMenuProps) => {
-    const [currentInstance,setCurrentInstance] = useState<number>(instances.findIndex((instance) => 
+const getInitialInstance = (instances: Instance[],query: string | undefined) => {
+    if(query){
+        const index = instances.findIndex((instance) => instance.instancia === query);
+        if(index !== -1){
+            return index;
+        }
+    }
+    const index = instances.findIndex((instance) => 
     {
         let date = new Date(instance.fecha);
         date.setDate(date.getDate() - 1);
         return date > new Date();
-    }));
+    });
+    if(index !== -1){
+        return index;
+    }
+    return 0;
+}
+
+const Instances = ({competition,instances}: InstanceMenuProps) => {
+    const router = useRouter();
+    const {query} = router
+    const [currentInstance,setCurrentInstance] = useState<number>(getInitialInstance(instances,query.instancia as string));
+    useEffect(() => {
+        if(query.instancia){
+            setCurrentInstance(getInitialInstance(instances,query.instancia as string));
+        }
+    },[query.instancia,instances])
     return(
         <>
         <h1 className={styles.title}>Instancias</h1>
