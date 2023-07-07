4f90f392cc0ba4fa5b40ce74d74ba13be3895df3
Julián Zylber <julian.zylber@ort.edu.ar>
2023-07-07 16:32:25 -0300

    fix(instances): minor typos

--- components/Instances/Provincial.tsx
+++ components/Instances/Provincial.tsx
@@ -15,8 +15,8 @@ export interface ProvincialParticipant {
 const Provincial = ({competition}: ProvincialProps) => {
     return(
         <>
-        <p className={styles.text}>La instancia provincial es <span className={styles.bold}>OPCIONAL</span>. Esto quiere decir que tanto participantes que hayan clasificado al provincial y no vayan, como paraticipantes que hayan aprobado la instancia zonal pero no hayan clasificado al provincial pueden seguir participando en OMA en la isntancia regional</p>
-        <p className={styles.text}>Los participantes que pueden participar de la provincial son aquellos que sumen 5 puntos entre las instancias Zonal e {competition === "OMA"?"Intercolegial":"Interescolar"}</p>
+        <p className={styles.text}>La instancia provincial es <span className={styles.bold}>OPCIONAL</span>. Esto quiere decir que tanto participantes que hayan clasificado al provincial y no vayan, como participantes que hayan aprobado la instancia zonal pero no hayan clasificado al provincial pueden seguir participando en OMA en la instancia regional.</p>
+        <p className={styles.text}>Los participantes que pueden participar de la provincial son aquellos que sumen 5 puntos entre las instancias Zonal e {competition === "OMA"?"Intercolegial":"Interescolar"}.</p>
         <h3 className={styles.subtitle}>Clasificados a Provincial</h3>
         </>
     )
