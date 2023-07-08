2654bc86ced0e93b9842ba2a24231c40f28bace1
Julián Zylber <julian.zylber@ort.edu.ar>
2023-07-07 23:30:59 -0300

    feat(collapsable): basic collapsable

---
+++ components/Collapsable/Collapsable.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import styles from "./Collapsable.module.scss";
+import Image from "next/image";
+
+interface CollapsableProps {
+    title: string,
+    children: React.ReactNode
+}
+
+const Collapsable = ({title,children}:CollapsableProps) => {
+    const [open,setOpen] = useState<boolean>(false);
+    return (
+    <section>
+        <div className={styles.header} onClick={()=>setOpen(!open)}>
+            <h3>{title}</h3>
+            <div className={[styles.arrow,open?styles.rotated:""].join(" ")}>
+                <Image src="/images/menuArrow.svg" alt="arrow" width={20} height={20}/>
+            </div>
+        </div>
+        {open && 
+        <div className={styles.content}>
+            {children}
+        </div>}
+    </section>
+  );
+};
+
+export default Collapsable;
