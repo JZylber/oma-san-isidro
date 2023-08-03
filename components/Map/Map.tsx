85153355dda16c061f00be3ed88fc82ace4bb50f
JZylber <zylberjulian@gmail.com>
2023-08-03 17:08:44 -0300

    feat(map): participant enlargement

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -20,6 +20,18 @@ const data : Array<Array<Array<MapItem>>> = [
         [{school: new School("Pilgrims College","San Isidro"),level:2},{school: new School("Santa Maria de las Lomas"),level:1}],
         [{school: new School("Michael Ham","Vicente López"),level:2},{school: new School("San Lucas","Olivos"),level:1}]
     ],
+    [   
+        [{school: new School("Participación Individual"),level:3}],
+        [{school: new School("San Gregorio"),level:3}]
+    ],
+    [   
+        [{school: new School("Michael Ham","Nordelta"),level:2}],
+        [{school: new School("Northlands","Olivos"),level:2},{school: new School("San Carlos"),level:1},{school: new School("El Buen Ayre"),level:3}]
+    ],
+    [   
+        [{school: new School("Los Molinos"),level:2},{school: new School("La Asunción de la Virgen"),level:1},{school: new School("Newman"),level:3}],
+        [{school: new School("Northlands","Olivos"),level:2},{school: new School("Los Molinos"),level:1},{school: new School("Cardenal Pironio"),level:3},{school: new School("Holy Cross","Mujeres"),level:2}]
+    ],
 ]
 
 const Map = ({competition}:MapProps) => {
