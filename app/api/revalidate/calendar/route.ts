fa79b3cb199ba287cf78958d8443f4f5ef1813f3
JZylber <zylberjulian@gmail.com>
2023-08-04 12:21:35 -0300

    feat(revalidation): home path

--- app/api/revalidate/calendar/route.ts
+++ app/api/revalidate/calendar/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
     try {
         revalidatePath('/otros/calendario');
-        revalidatePath('');
+        revalidatePath('/');
         return NextResponse.json({revalidated: true})
       } catch (err) {
         return NextResponse.json({message:'Error revalidating'}, {status: 500});
