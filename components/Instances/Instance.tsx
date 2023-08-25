b797411d4433d77811a6f118f2f263c5ef71f84c
JZylber <zylberjulian@gmail.com>
2023-08-25 18:07:40 -0300

    feat(provincial): oma version

--- components/Instances/Instance.tsx
+++ components/Instances/Instance.tsx
@@ -49,28 +49,13 @@ export interface VenueInput{
 
 const months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
 
-const displayInstance = (instance: string, competition: string, instanceData: RegionalInstance | ProvincialInstance | undefined) => {
-    if(instance === "PROVINCIAL" &&  instanceData && "participants" in instanceData){
-        const provincialInstance = instanceData as ProvincialInstance;
-        const participants : ProvincialParticipant[] = provincialInstance.participants.map((participant) => {return{nivel: participant.nivel, participante: new Participant(participant.nombre,participant.apellido), colegio: new School(participant.colegio.nombre,participant.colegio.sede)}});
-        return <Provincial competition={competition} participants={participants} auth_max_date={provincialInstance.auth_max_date}/>
-    }
-    else if(instance === "NACIONAL"){
-        return <span className={styles.text}>Proximamente...</span>
-    }
-    else{
-        return <span className={styles.text}>Proximamente...</span>
-    }
-};
-
 const InstanceData = ({competition,instance}:InstanceProps) => {
     const {instancia,fecha} = instance;
     return(
         <>
         <h2 className={styles.title}>{instancia[0] + instancia.slice(1).toLocaleLowerCase()}</h2>
         <h3 className={styles.subtitle}>{`${fecha.getUTCDate()} de ${months[fecha.getMonth()]}`}</h3>
-        {instancia === "PROVINCIAL" && competition=="OMA" && <span className={styles.text}>Proximamente...</span>}
-        {instancia === "PROVINCIAL" && competition=="ÑANDÚ" && <ProvincialInfo competition={competition}/>}
+        {instancia === "PROVINCIAL" && <ProvincialInfo competition={competition}/>}
         {instancia === "NACIONAL" && <span className={styles.text}>Proximamente...</span>}
         {instancia !== "PROVINCIAL" && instancia !== "NACIONAL" && 
             <VenueInfo 
