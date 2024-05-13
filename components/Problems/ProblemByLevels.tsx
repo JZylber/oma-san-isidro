da4d1c4fc624ccee9138545c2e2ae5a18296a0df
JZylber <zylberjulian@gmail.com>
2024-05-13 10:27:59 -0300

    Basic Problem Levels

---
+++ components/Problems/ProblemByLevels.tsx
@@ -0,0 +1,7 @@
+import styles from "./ProblemLevels.module.scss";
+
+const ProblemLevels : ({problem_links,close}: {problem_links: string [], close: () => void}) => JSX.Element = ({problem_links,close}) => {
+    return(<div className="container">test</div>)
+}
+
+export default ProblemLevels;
--- components/Problems/Problems.tsx
+++ components/Problems/Problems.tsx
@@ -1,22 +1,34 @@
+import { useState } from "react";
 import styles from "./Problems.module.scss";
+import ProblemLevels from "./ProblemByLevels";
 
 const Problems : ({type}: {type: string}) => JSX.Element = ({type}) => {  
-    const problems : {[key: string]: {[key:string]: string}}= require(`../../data/Problemas${type}.json`);
+    const problems : {[key: string]: {[key:string]: string | string[]}}= require(`../../data/Problemas${type}.json`);
+    const [openLevels, setOpenLevels] = useState<{year: string, instance: string} | undefined>(undefined);
     return(
     <>
         <div className={styles.type}>{type == "OMA"?"Oma":"Ñandú"}</div>
         <h1 className={styles.title}>Problemas</h1>
-        {Object.keys(problems).sort().reverse().map((key:string,idx:number) => {
-            const problem = problems[key];
+        {Object.keys(problems).sort().reverse().map((year:string,idx:number) => {
+            const problem = problems[year];
             return(
                 <section className={styles.year_problems} key={idx}>
-                    <h2 className={styles.year_number}>{key}</h2>
+                    <h2 className={styles.year_number}>{year}</h2>
                     <div className={styles.year_links}>
-                    {Object.keys(problem).map((key:string,idx:number) => {
-                        const problemLink = problem[key];
+                    {Object.keys(problem).map((instance:string,idx:number) => {
+                        const problemLink = problem[instance];
+                        if(problemLink instanceof Array){
+                            return(<>
+                            <div className={styles.link} key={idx} onClick={() => setOpenLevels({year:year,instance:instance})}>
+                                {instance}
+                                {openLevels?.year == year && openLevels?.instance == instance && 
+                                <ProblemLevels problem_links={problemLink} close={() => setOpenLevels(undefined)}/>}
+                            </div>
+                            </>)
+                        } else{
                         return(
-                            <a className={styles.link} href={problemLink} target="_blank" rel="noopener noreferrer" key={idx}>{key}</a>
-                        )
+                            <a className={styles.link} href={problemLink} target="_blank" rel="noopener noreferrer" key={idx} onClick={() => setOpenLevels({year:year,instance:instance})}>{instance}</a>
+                        )}
                     })}
                     </div>
                 </section>
