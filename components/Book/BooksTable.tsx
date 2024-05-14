4a3fdc288524a3a481369cc1d61c50e332d9acb2
JZylber <zylberjulian@gmail.com>
2024-05-13 22:09:40 -0300

    feat(books): filter component

--- components/Book/BooksTable.tsx
+++ components/Book/BooksTable.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
 import type {Book} from "../../app/(public)/otros/libros/libros";
 
 const BooksTable = ({books}:{books:Book[]}) => {
     return (
+        <>
+        <div className="border-2 border-solid border-black rounded-xl font-unbounded font-[500] text-[1.6rem] mt-6 w-1/3">
+            <div className="flex justify-between items-center p-4">
+                <Image src="/images/filter.svg" alt="" width={30} height={30}/>
+                <span>Filtrar</span>
+                <Image className="" src="/images/menuSelectIcon.svg" alt="" width={20} height={20}/>
+            </div>
+        </div>
         <div className="border-2 border-solid border-black rounded-xl overflow-hidden mt-6">
             <table className="">
                 <thead className="font-unbounded text-[1.6rem] bg-primary-light-blue border-b-2 border-solid border-black">
@@ -28,6 +37,7 @@ const BooksTable = ({books}:{books:Book[]}) => {
                 </tbody>
             </table>
         </div>
+        </>
     );
 }
 
