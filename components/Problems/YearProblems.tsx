0b53b8b37127d91a0273fa74e31773bebc776c67
JZylber <zylberjulian@gmail.com>
2024-05-24 22:22:33 -0300

    feat(problems): switched dropdown arrow

--- components/Problems/YearProblems.tsx
+++ components/Problems/YearProblems.tsx
@@ -6,9 +6,10 @@ const InstanceProblems : ({instance,problem_links}: {instance:string,problem_lin
     return(
         <div className="flex flex-col gap-y-4">
             <div className="flex gap-x-4 cursor-pointer" onClick={() => setOpen(!open)}>
-                <span className="font-unbounded font-[400] text-[1.6rem] w-60">{instance}</span><Image src="/images/menuArrow.svg" alt="arrow" width={10} height={10} className={`${open?"rotate-90":""} transition-transform`}/>
+                <Image src="/images/menuArrow.svg" alt="arrow" width={10} height={10} className={`${open?"rotate-90":""} transition-transform`}/>
+                <span className="font-unbounded font-[400] text-[1.6rem] w-60">{instance}</span>
             </div>
-            {open && <div className="flex flex-col gap-y-4 pl-20">
+            {open && <div className="flex flex-col gap-y-4 pl-12">
                 {problem_links.map((problem:string,idx:number) => {
                     return(
                         <a key={idx} href={problem} target="_blank" rel="noreferrer" className="cursor-pointer">
@@ -25,8 +26,9 @@ const YearProblems : ({year,problem_links}: {year:number,problem_links: Record<s
     const [open,setOpen] = useState(false);
     return(
         <div className="flex flex-col">
-            <div className="flex gap-x-4 cursor-pointer" onClick={() => setOpen(!open)}>
-                <span className="font-unbounded font-[500] text-[2.4rem] w-40">{year}</span><Image src="/images/menuArrow.svg" alt="arrow" width={14} height={14} className={`${open?"rotate-90":""} transition-transform`}/>
+            <div className="flex gap-x-6 cursor-pointer" onClick={() => setOpen(!open)}>
+                <Image src="/images/menuArrow.svg" alt="arrow" width={12} height={12} className={`${open?"rotate-90":""} transition-transform`}/>
+                <span className="font-unbounded font-[500] text-[2.4rem] w-40">{year}</span>
             </div>
             {open && <div className="flex flex-col gap-y-4 pl-12">
                 {Object.keys(problem_links).map((problem:string,idx:number) => {
