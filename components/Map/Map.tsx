c2031ffa5a34b520f59953ac6c96a3e080208001
JZylber <zylberjulian@gmail.com>
2024-09-12 16:29:59 -0300

    feat(map): map imports

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -8,108 +8,169 @@ import MapData from "./MapFromJson";
 import ParticipantTable from "./Table/Table";
 
 interface MapProps {
-   competition: Competition;
+  competition: Competition;
 }
 
 export interface MapItem extends FilterableObject {
-    school: School;
-    level: number;
+  school: School;
+  level: number;
 }
 
-
-const countParticipantsOfLevel = (participants: MapItem[],level:number) => {
-    return(participants.filter((item) => item.level === level).length);
-}
-
-const participantsInColumn = (column:MapItem[],selected: MapItem[]) => {
-    const selectedInColumn = selected.filter((item) => column.includes(item));
-    const participantsPerLevel = [0,1,2,3].map((level) => countParticipantsOfLevel(selectedInColumn,level));
-    return(participantsPerLevel);
+const countParticipantsOfLevel = (participants: MapItem[], level: number) => {
+  return participants.filter((item) => item.level === level).length;
 };
 
-const Map = ({competition}:MapProps) => {
-    const data = MapData("Regional",competition);
-    const flattenedData = data.flat(2);
-    const columns = data.map((column) => column.flat(1));
-    const [schoolFilter,updateFilter,filtered_schools,options] = useFilter(flattenedData);
-    const participantsPerColumn = columns.map((column,index) => [index + 1,...participantsInColumn(column,filtered_schools)]).filter((column) => column.slice(1).some((value) => value !== 0));
-    const someSelected = Object.values(schoolFilter).some((value) => value !== undefined);
-    const isSelected = (item:MapItem) => {
-        return(someSelected && filtered_schools.includes(item))
-    }
-    const freePlacesSelected = schoolFilter.school?.name === "LIBRE";
-    return(
-        <>
-        <div style={{margin:"2rem 0rem 2rem 0rem"}}>
-            <TransformWrapper
-                initialScale={0.75}
-                minScale={0.2}
-                maxScale={2}
-            >
-                <TransformComponent wrapperClass={styles.gridContainer}>
-                <div className={styles.grid}>
-                    {data.map((column,column_index) => {
-                        const columnIsSelected = someSelected && participantsPerColumn.some((column) => column[0] === column_index + 1);
-                        return(
-                            <div key={column_index} className={[styles.column,columnIsSelected?styles.selected:""].join(" ")}>
-                                <div className={styles.columnHeader}>
-                                    <div>{column_index+1}</div>
-                                </div>
-                                {column.map((row, row_index) => {
-                                    return(<ParticipantTable key={`${column_index}-${row_index}`} participants={row} isSelected={isSelected}/>)
-                                })}
-                            </div>
-                        )
-                    })
-                    }
-                </div>
-                </TransformComponent>
-            </TransformWrapper>
-        </div>
-        <form className={styles.form}>
-            <SelectResultCategory category="Colegio" value={schoolFilter.school} setValue={(option?: School) => updateFilter({school: option})} options={options.school} input={true}/>
-            <SelectResultCategory category="Nivel" value={schoolFilter.level} setValue={(option?: number) => updateFilter({level: option})} options={options.level.filter((option) => option!==0).sort()} clear={true}/>
-        </form>
-        <div className={styles.values}>
-            <table className={styles.values_table}>
-                <thead>
-                    <tr>
-                        <th style={{width: "17.5%"}}>Fila</th>
-                        {!freePlacesSelected ?
-                        <> 
-                        <th style={{width: "27.5%"}}>Nivel 1</th>
-                        <th style={{width: "27.5%"}}>Nivel 2</th>
-                        <th style={{width: "27.5%"}}>Nivel 3</th>
-                        </>:
-                        <th style={{width: "82.5%"}}>Lugares</th>
-                        }
+const participantsInColumn = (column: MapItem[], selected: MapItem[]) => {
+  const selectedInColumn = selected.filter((item) => column.includes(item));
+  const participantsPerLevel = [0, 1, 2, 3].map((level) =>
+    countParticipantsOfLevel(selectedInColumn, level)
+  );
+  return participantsPerLevel;
+};
+
+const Map = ({ competition }: MapProps) => {
+  const data = MapData("Regional", competition);
+  const flattenedData = data.flat(2);
+  const columns = data.map((column) => column.flat(1));
+  const [schoolFilter, updateFilter, filtered_schools, options] =
+    useFilter(flattenedData);
+  const participantsPerColumn = columns
+    .map((column, index) => [
+      index + 1,
+      ...participantsInColumn(column, filtered_schools),
+    ])
+    .filter((column) => column.slice(1).some((value) => value !== 0));
+  const someSelected = Object.values(schoolFilter).some(
+    (value) => value !== undefined
+  );
+  const isSelected = (item: MapItem) => {
+    return someSelected && filtered_schools.includes(item);
+  };
+  const freePlacesSelected = schoolFilter.school?.name === "LIBRE";
+  return (
+    <>
+      <div style={{ margin: "2rem 0rem 2rem 0rem" }}>
+        <TransformWrapper initialScale={0.75} minScale={0.2} maxScale={2}>
+          <TransformComponent wrapperClass={styles.gridContainer}>
+            <div className={styles.grid}>
+              {data.map((column, column_index) => {
+                const columnIsSelected =
+                  someSelected &&
+                  participantsPerColumn.some(
+                    (column) => column[0] === column_index + 1
+                  );
+                return (
+                  <div
+                    key={column_index}
+                    className={[
+                      styles.column,
+                      columnIsSelected ? styles.selected : "",
+                    ].join(" ")}
+                  >
+                    <div className={styles.columnHeader}>
+                      <div>{column_index + 1}</div>
+                    </div>
+                    {column.map((row, row_index) => {
+                      return (
+                        <ParticipantTable
+                          key={`${column_index}-${row_index}`}
+                          participants={row}
+                          isSelected={isSelected}
+                        />
+                      );
+                    })}
+                  </div>
+                );
+              })}
+            </div>
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
+      <form className={styles.form}>
+        <SelectResultCategory
+          category="Colegio"
+          value={schoolFilter.school}
+          setValue={(option?: School) => updateFilter({ school: option })}
+          options={options.school}
+          input={true}
+        />
+        <SelectResultCategory
+          category="Nivel"
+          value={schoolFilter.level}
+          setValue={(option?: number) => updateFilter({ level: option })}
+          options={options.level.filter((option) => option !== 0).sort()}
+          clear={true}
+        />
+      </form>
+      <div className={styles.values}>
+        <table className={styles.values_table}>
+          <thead>
+            <tr>
+              <th style={{ width: "17.5%" }}>Fila</th>
+              {!freePlacesSelected ? (
+                <>
+                  <th style={{ width: "27.5%" }}>Nivel 1</th>
+                  <th style={{ width: "27.5%" }}>Nivel 2</th>
+                  <th style={{ width: "27.5%" }}>Nivel 3</th>
+                </>
+              ) : (
+                <th style={{ width: "82.5%" }}>Lugares</th>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {freePlacesSelected ? (
+              <>
+                {participantsPerColumn.map((column, column_index) => {
+                  return (
+                    <tr key={column_index}>
+                      {column.slice(0, 2).map((value, row_index) => {
+                        return (
+                          <td
+                            className={[
+                              styles.center_align,
+                              row_index === 0 ? styles.row : "",
+                            ].join(" ")}
+                            key={row_index}
+                          >
+                            {value !== 0 ? value : ""}
+                          </td>
+                        );
+                      })}
                     </tr>
-                </thead>
-                <tbody>
-                    {freePlacesSelected?
-                    <>{participantsPerColumn.map((column,column_index) => {
-                        return(
-                            <tr key={column_index}>
-                                {column.slice(0,2).map((value,row_index) => {
-                                    return(<td className={[styles.center_align,row_index === 0?styles.row:""].join(" ")} key={row_index}>{value !== 0?value:""}</td>)
-                                })}
-                            </tr>
-                        )
-                    })}</>
-                    :<>{participantsPerColumn.map((column,column_index) => {
-                        return(
-                            <tr key={column_index}>
-                                {[column[0]].concat(column.slice(2)).map((value,row_index) => {
-                                    return(<td className={[styles.center_align,row_index === 0?styles.row:""].join(" ")} key={row_index}>{value !== 0?value:""}</td>)
-                                })}
-                            </tr>
-                        )
-                    })}</>
-                    }
-                </tbody>
-            </table>
-        </div>
-        </>
-    )};
+                  );
+                })}
+              </>
+            ) : (
+              <>
+                {participantsPerColumn.map((column, column_index) => {
+                  return (
+                    <tr key={column_index}>
+                      {[column[0]]
+                        .concat(column.slice(2))
+                        .map((value, row_index) => {
+                          return (
+                            <td
+                              className={[
+                                styles.center_align,
+                                row_index === 0 ? styles.row : "",
+                              ].join(" ")}
+                              key={row_index}
+                            >
+                              {value !== 0 ? value : ""}
+                            </td>
+                          );
+                        })}
+                    </tr>
+                  );
+                })}
+              </>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
 export default Map;
