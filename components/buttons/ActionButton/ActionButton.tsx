c3c0f8a584d1db730772010a7fd9602a11fb449f
JZylber <zylberjulian@gmail.com>
2024-10-03 11:55:54 -0300

    feat(dashboard): loader

--- components/buttons/ActionButton/ActionButton.tsx
+++ components/buttons/ActionButton/ActionButton.tsx
@@ -2,18 +2,20 @@ interface ActionButtonProps {
   children: React.ReactNode;
   onClick: () => void;
   important?: boolean;
+  className?: string;
 }
 
 const ActionButton = ({
   children,
   important = false,
   onClick,
+  className = "",
 }: ActionButtonProps) => {
   return (
     <button
       className={`flex justify-center items-center border-2 border-b-4 border-black rounded-xl w-36 h-16 font-montserrat text-2xl ${
         important ? "bg-primary-light-blue" : ""
-      }`}
+      } ${className}`}
       onClick={onClick}
     >
       {children}
