d412fceb4125e0ccc4d5dacff85e8f5216dae4a1
JZylber <zylberjulian@gmail.com>
2024-10-03 11:58:41 -0300

    fix(dashboard): loader

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -2,7 +2,7 @@ import Loader from "components/Loader/Loader";
 
 export default function Loading() {
   return (
-    <div className="grow h-full w-full items-center justify-center">
+    <div className="grow flex h-full w-full items-center justify-center">
       <Loader />
     </div>
   );
