7d5d3f2509e75d77352cf7038fcdf64117805c37
JZylber <zylberjulian@gmail.com>
2024-10-25 17:35:51 -0300

    feat(server): create and add many

--- server/routers/dashboard.ts
+++ server/routers/dashboard.ts
@@ -1,79 +1,165 @@
 import { protectedProcedure, router } from "../trpc";
-import { z } from 'zod';
+import { z } from "zod";
 import { INSTANCE } from "../types";
-import { deleteResult, getParticipants, getPassingParticipants, getPreviousInstance, getProvincialParticipants, getResults, modifyResult, newResult, Participant} from "./results/results_db_calls";
+import {
+  createResults,
+  deleteResult,
+  getParticipants,
+  getPassingParticipants,
+  getPreviousInstance,
+  getProvincialParticipants,
+  getResults,
+  modifyResult,
+  newResult,
+  Participant,
+  updateResults,
+} from "./results/results_db_calls";
 import { INSTANCIA } from "@prisma/client";
+import { create } from "domain";
 
-const getEditableResults = async (competencia: string, año: number, instancia: INSTANCIA) => {
-        const prevInstance = getPreviousInstance(competencia,instancia) as INSTANCIA;
-        let participants: Participant[];
-        if(prevInstance) participants = await getPassingParticipants(competencia,año,prevInstance)
-        if(!prevInstance && (instancia === "INTERCOLEGIAL" || instancia === "INTERESCOLAR")){
-            participants = await getParticipants(competencia,año);
-        }
-        if(!prevInstance && instancia === "PROVINCIAL" ) {
-            participants = await getProvincialParticipants(competencia,año);
-        } 
-        const results = await getResults(competencia,año,instancia);
-        const participantsWithResults = participants!.map(participant => {
-            const result = results.find(result => result.participacion.id_participacion === participant.id_participacion);
-            if (result) {
-                return {
-                    ...participant,
-                    resultados: {
-                        puntaje: result.resultados as string[],
-                        aprobado: result.aprobado,
-                        presente: result.presente,
-                        aclaracion: result.aclaracion,
-                    },
-                    id_rinde: result.id_rinde,
-                };
-            } else {
-                return {
-                    ...participant,
-                    resultados: null,
-                    id_rinde: null,
-                };
-            }
-        })
-        return participantsWithResults;
-}
+const getEditableResults = async (
+  competencia: string,
+  año: number,
+  instancia: INSTANCIA
+) => {
+  const prevInstance = getPreviousInstance(competencia, instancia) as INSTANCIA;
+  let participants: Participant[];
+  if (prevInstance)
+    participants = await getPassingParticipants(competencia, año, prevInstance);
+  if (
+    !prevInstance &&
+    (instancia === "INTERCOLEGIAL" || instancia === "INTERESCOLAR")
+  ) {
+    participants = await getParticipants(competencia, año);
+  }
+  if (!prevInstance && instancia === "PROVINCIAL") {
+    participants = await getProvincialParticipants(competencia, año);
+  }
+  const results = await getResults(competencia, año, instancia);
+  const participantsWithResults = participants!.map((participant) => {
+    const result = results.find(
+      (result) =>
+        result.participacion.id_participacion === participant.id_participacion
+    );
+    if (result) {
+      return {
+        ...participant,
+        resultados: {
+          puntaje: result.resultados as string[],
+          aprobado: result.aprobado,
+          presente: result.presente,
+          aclaracion: result.aclaracion,
+        },
+        id_rinde: result.id_rinde,
+      };
+    } else {
+      return {
+        ...participant,
+        resultados: null,
+        id_rinde: null,
+      };
+    }
+  });
+  return participantsWithResults;
+};
 
-export type EditableResult =  Awaited<ReturnType<typeof getEditableResults>>[0];
-
+export type EditableResult = Awaited<ReturnType<typeof getEditableResults>>[0];
 
 export const dashboardRouter = router({
-    getResults: protectedProcedure.input(z.object({
+  getResults: protectedProcedure
+    .input(
+      z.object({
         año: z.number(),
         instancia: INSTANCE,
         competencia: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { año, instancia, competencia } = input;
+      return getEditableResults(competencia, año, instancia);
     }),
-    ).query(async ({ctx,input}) => {
-        const {año, instancia, competencia} = input;
-        return getEditableResults(competencia,año,instancia);
-}),
-    editResult: protectedProcedure.input(z.object({
+  editResult: protectedProcedure
+    .input(
+      z.object({
         id_rinde: z.number(),
         puntaje: z.array(z.string()),
         aprobado: z.boolean(),
         presente: z.boolean(),
         aclaracion: z.string().nullable(),
-    })).mutation(async ({ctx,input}) => {
-        const {id_rinde, puntaje, aprobado, presente, aclaracion} = input;
-        return await modifyResult(id_rinde, puntaje, aprobado, presente, aclaracion);
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id_rinde, puntaje, aprobado, presente, aclaracion } = input;
+      return await modifyResult(
+        id_rinde,
+        puntaje,
+        aprobado,
+        presente,
+        aclaracion
+      );
     }),
-    newResult: protectedProcedure.input(z.object({
+  newResult: protectedProcedure
+    .input(
+      z.object({
         id_participacion: z.number(),
         id_prueba: z.number(),
         puntaje: z.array(z.string()),
         aprobado: z.boolean(),
         presente: z.boolean(),
         aclaracion: z.string().nullable(),
-    })).mutation(async ({ctx,input}) => {
-        const {id_participacion, id_prueba, puntaje, aprobado, presente, aclaracion} = input;
-        return await newResult(id_participacion, id_prueba, puntaje, aprobado, presente, aclaracion);
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const {
+        id_participacion,
+        id_prueba,
+        puntaje,
+        aprobado,
+        presente,
+        aclaracion,
+      } = input;
+      return await newResult(
+        id_participacion,
+        id_prueba,
+        puntaje,
+        aprobado,
+        presente,
+        aclaracion
+      );
     }),
-    deleteResult: protectedProcedure.input(z.number()).mutation(async ({ctx,input}) => {
-        return await deleteResult(input);
+  deleteResult: protectedProcedure
+    .input(z.number())
+    .mutation(async ({ ctx, input }) => {
+      return await deleteResult(input);
+    }),
+  updateResults: protectedProcedure
+    .input(
+      z.object({
+        id_prueba: z.number(),
+        Results2Update: z.array(
+          z.object({
+            id_rinde: z.number(),
+            resultados: z.array(z.string()),
+            aprobado: z.boolean(),
+            presente: z.boolean(),
+            aclaracion: z.string().nullable(),
+          })
+        ),
+        Results2Add: z.array(
+          z.object({
+            dni: z.number(),
+            resultados: z.array(z.string()),
+            aprobado: z.boolean(),
+            presente: z.boolean(),
+            aclaracion: z.string().nullable(),
+          })
+        ),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id_prueba, Results2Update, Results2Add } = input;
+      const updatePromise = updateResults(Results2Update);
+      const addPromise = createResults(Results2Add, id_prueba);
+      await Promise.all([updatePromise, addPromise]);
     }),
 });
--- server/routers/results/results_db_calls.ts
+++ server/routers/results/results_db_calls.ts
@@ -1,91 +1,101 @@
 import { INSTANCIA } from "@prisma/client";
 import { prisma } from "../../db";
 
-export const getPreviousInstance =  (competition: string,instance: INSTANCIA) => {
-    if(instance === 'NACIONAL') return 'REGIONAL' as INSTANCIA;
-    if(instance === 'REGIONAL') return 'ZONAL' as INSTANCIA;
-    if(instance === 'ZONAL'){
-      if(competition === "OMA") return 'INTERCOLEGIAL' as INSTANCIA;
-      else return 'INTERESCOLAR' as INSTANCIA;
-    };
-    return undefined;
+export const getPreviousInstance = (
+  competition: string,
+  instance: INSTANCIA
+) => {
+  if (instance === "NACIONAL") return "REGIONAL" as INSTANCIA;
+  if (instance === "REGIONAL") return "ZONAL" as INSTANCIA;
+  if (instance === "ZONAL") {
+    if (competition === "OMA") return "INTERCOLEGIAL" as INSTANCIA;
+    else return "INTERESCOLAR" as INSTANCIA;
   }
+  return undefined;
+};
 
-  export const getParticipants = async (competition: string,year: number) => {
-    const query = await prisma.participacion.findMany({
-      where: {
-        competencia: {
-          tipo: competition,
-          ano: year
-        }
+export const getParticipants = async (competition: string, year: number) => {
+  const query = await prisma.participacion.findMany({
+    where: {
+      competencia: {
+        tipo: competition,
+        ano: year,
       },
-      orderBy: [
-        {nivel: 'asc'},
-        {
-          participante: {
-            apellido: 'asc'
-          }
-        },
-        {
-          participante: {
-            nombre: 'asc'
-          }
-        }
-      ],
-      select: {
-        id_participacion: true,
+    },
+    orderBy: [
+      { nivel: "asc" },
+      {
         participante: {
-          select: {
-            nombre: true,
-            apellido: true,
-            dni: true
-          }
+          apellido: "asc",
         },
-        colegio: {
-          select: {
-            nombre: true,
-            sede: true
-          }
+      },
+      {
+        participante: {
+          nombre: "asc",
         },
-        nivel: true
-      }
-    });
-    const results = query;
-    return (results);
-  };
-  
-  export const getPassingParticipants = async (type: string, year: number, instance: INSTANCIA) => {
-    const query = await prisma.prueba.findFirst({
+      },
+    ],
+    select: {
+      id_participacion: true,
+      participante: {
+        select: {
+          nombre: true,
+          apellido: true,
+          dni: true,
+        },
+      },
+      colegio: {
+        select: {
+          nombre: true,
+          sede: true,
+        },
+      },
+      nivel: true,
+    },
+  });
+  const results = query;
+  return results;
+};
+
+export const getPassingParticipants = async (
+  type: string,
+  year: number,
+  instance: INSTANCIA
+) => {
+  const query = await prisma.prueba.findFirst({
     where: {
       competencia: {
         tipo: type,
-        ano: year
+        ano: year,
       },
       instancia: instance,
     },
     select: {
       rinden: {
         where: {
-          aprobado: true
+          aprobado: true,
         },
         orderBy: [
-        {participacion: {
-          nivel: 'asc'
-        }},  
-        {
-          participacion: {
-            participante: {
-              apellido: 'asc'
-            }
-          }
-        },
-        {
-          participacion: {
-            participante: {
-              nombre: 'asc'
-            }
-          }
-        }],
+          {
+            participacion: {
+              nivel: "asc",
+            },
+          },
+          {
+            participacion: {
+              participante: {
+                apellido: "asc",
+              },
+            },
+          },
+          {
+            participacion: {
+              participante: {
+                nombre: "asc",
+              },
+            },
+          },
+        ],
         select: {
           participacion: {
             select: {
@@ -94,79 +104,90 @@ export const getPreviousInstance =  (competition: string,instance: INSTANCIA) =>
                 select: {
                   nombre: true,
                   apellido: true,
-                    dni: true
-                }
+                  dni: true,
+                },
               },
               colegio: {
                 select: {
                   nombre: true,
-                  sede: true
-                }
+                  sede: true,
+                },
               },
-              nivel: true
-            }
-          }
-        }
-      }
-    }
-    });
-    const results = query?query.rinden.map((participation) => participation.participacion):[];
-    return (results);
-  };
-
-  export const getInstanceData = async (year: number,competition: string, instance: INSTANCIA) => {
-    const query = await prisma.prueba.findFirst({
-      where: {
-        competencia: {
-          tipo : competition,
-          ano: year
+              nivel: true,
+            },
+          },
         },
-        instancia: instance
-        
+      },
+    },
+  });
+  const results = query
+    ? query.rinden.map((participation) => participation.participacion)
+    : [];
+  return results;
+};
+
+export const getInstanceData = async (
+  year: number,
+  competition: string,
+  instance: INSTANCIA
+) => {
+  const query = await prisma.prueba.findFirst({
+    where: {
+      competencia: {
+        tipo: competition,
+        ano: year,
+      },
+      instancia: instance,
     },
     select: {
       fecha_limite_autorizacion: true,
       hora_ingreso: true,
       duracion: true,
-      criterio_habilitacion: true
-    }  
-    })
-    return (query);
-  }
-  
+      criterio_habilitacion: true,
+    },
+  });
+  return query;
+};
 
-  const getPassingParticipantsWithScore = async (type: string, year: number, instance: INSTANCIA) => {
-    const query = await prisma.prueba.findFirst({
+const getPassingParticipantsWithScore = async (
+  type: string,
+  year: number,
+  instance: INSTANCIA
+) => {
+  const query = await prisma.prueba.findFirst({
     where: {
       competencia: {
         tipo: type,
-        ano: year
+        ano: year,
       },
       instancia: instance,
     },
     select: {
       rinden: {
         where: {
-          aprobado: true
+          aprobado: true,
         },
         orderBy: [
-        {participacion: {
-          nivel: 'asc'
-        }},  
-        {
-          participacion: {
-            participante: {
-              apellido: 'asc'
-            }
-          }
-        },
-        {
-          participacion: {
-            participante: {
-              nombre: 'asc'
-            }
-          }
-        }],
+          {
+            participacion: {
+              nivel: "asc",
+            },
+          },
+          {
+            participacion: {
+              participante: {
+                apellido: "asc",
+              },
+            },
+          },
+          {
+            participacion: {
+              participante: {
+                nombre: "asc",
+              },
+            },
+          },
+        ],
         select: {
           resultados: true,
           participacion: {
@@ -176,141 +197,280 @@ export const getPreviousInstance =  (competition: string,instance: INSTANCIA) =>
                 select: {
                   nombre: true,
                   apellido: true,
-                  dni: true
-                }
+                  dni: true,
+                },
               },
               colegio: {
                 select: {
                   nombre: true,
-                  sede: true
-                }
+                  sede: true,
+                },
               },
-              nivel: true
-            }
-          }
-        }
-      }
-    }
-    });
-    const results = query?query.rinden.map((participation) => {return{...participation.participacion,resultados: participation.resultados}}):[];
-    return (results);
-  };
-
-  export const getDisabled = async (competition: string, year: number, instance: string) => {
-    const query = await prisma.inhabilitado.findMany({
-      where: {
-        Prueba: {
-          competencia: {
-            tipo: competition,
-            ano: year
+              nivel: true,
+            },
           },
-          instancia: instance as INSTANCIA
-      }
-      },
-      select: {
-        id_participacion: true
-      }
-    })
-    return (query);
-  }
-
-export const getProvincialParticipants = async (competition: string, year: number) => {
-  const {interescolar,zonal,data} = await Promise.all([getPassingParticipantsWithScore(competition,year,competition === "OMA"?'INTERCOLEGIAL':'INTERESCOLAR'),getPassingParticipantsWithScore(competition,year,'ZONAL'),getInstanceData(year,competition,"PROVINCIAL")]).then(([interescolar, zonal, data]) => {return({interescolar: interescolar,zonal: zonal, data: data})});
-  let provincialParticipants = zonal.map((participant) => {
-    let interescolar_participant = interescolar.find((interescolar_participant) => interescolar_participant.id_participacion === participant.id_participacion);
-    let interescolar_points = interescolar_participant?.resultados?Number((interescolar_participant.resultados as string[])[3]):0;
-    return({...participant,puntos: interescolar_points + Number((participant.resultados as string[])[3])});
-  });
-  const criteria = data?.criterio_habilitacion?data.criterio_habilitacion as number[]:[5,5,5];
-  provincialParticipants = provincialParticipants.filter((participant) => participant.puntos >= criteria[participant.nivel - 1]);
-  const disabled = await getDisabled(competition,year,"PROVINCIAL");
-  provincialParticipants = provincialParticipants.filter((participant) => !disabled.some((disabled_participant) => disabled_participant.id_participacion === participant.id_participacion));
-  return provincialParticipants
-}
-
-export const  getResults = async (competition: string, year: number, instance: INSTANCIA) => {
-    const results = await prisma.rinde.findMany(
-        {   
-            where : {
-                    prueba : {
-                        AND : [
-                            {instancia : instance},
-                            {competencia : {
-                                AND : [
-                                    {ano : year},
-                                    {tipo: competition},
-                            ]}},
-                        ],
-                    },
-            },
-            orderBy: [
-                {participacion:{
-                    nivel: 'asc'
-                    }
-                },
-                {participacion:{
-                    participante: {
-                        apellido: 'asc'
-                    }}
-                }
-            ],
-            select : {
-                prueba: {
-                    select : {
-                        cantidad_problemas: true,
-                    }
-                },
-                id_rinde: true,
-                presente: true,
-                aprobado: true,
-                aclaracion: true,
-                resultados: true,
-                participacion : {
-                    select : {
-                        id_participacion: true,
-                    }
-                }
-            },
-        })
-    return results;
-  }
-
-export const modifyResult = async (id_rinde: number, score: string[], approved: boolean, present: boolean, clarification: string | null) => {
-    const result = await prisma.rinde.update({
-        where: {
-            id_rinde: id_rinde
         },
-        data: {
-            resultados: score,
-            aprobado: approved,
-            presente: present,
-            aclaracion: clarification
-        }
-    });
-    return result;
-}
+      },
+    },
+  });
+  const results = query
+    ? query.rinden.map((participation) => {
+        return {
+          ...participation.participacion,
+          resultados: participation.resultados,
+        };
+      })
+    : [];
+  return results;
+};
 
-export const newResult = async (id_participation: number, id_test: number, score: string[], approved: boolean, present: boolean, clarification: string | null) => {
-    const result = await prisma.rinde.create({
-        data: {
-            id_participacion: id_participation,
-            id_prueba: id_test,
-            resultados: score,
-            aprobado: approved,
-            presente: present,
-            aclaracion: clarification
-        }
-    });
-    return result;
-}
+export const getDisabled = async (
+  competition: string,
+  year: number,
+  instance: string
+) => {
+  const query = await prisma.inhabilitado.findMany({
+    where: {
+      Prueba: {
+        competencia: {
+          tipo: competition,
+          ano: year,
+        },
+        instancia: instance as INSTANCIA,
+      },
+    },
+    select: {
+      id_participacion: true,
+    },
+  });
+  return query;
+};
+
+export const getProvincialParticipants = async (
+  competition: string,
+  year: number
+) => {
+  const { interescolar, zonal, data } = await Promise.all([
+    getPassingParticipantsWithScore(
+      competition,
+      year,
+      competition === "OMA" ? "INTERCOLEGIAL" : "INTERESCOLAR"
+    ),
+    getPassingParticipantsWithScore(competition, year, "ZONAL"),
+    getInstanceData(year, competition, "PROVINCIAL"),
+  ]).then(([interescolar, zonal, data]) => {
+    return { interescolar: interescolar, zonal: zonal, data: data };
+  });
+  let provincialParticipants = zonal.map((participant) => {
+    let interescolar_participant = interescolar.find(
+      (interescolar_participant) =>
+        interescolar_participant.id_participacion ===
+        participant.id_participacion
+    );
+    let interescolar_points = interescolar_participant?.resultados
+      ? Number((interescolar_participant.resultados as string[])[3])
+      : 0;
+    return {
+      ...participant,
+      puntos:
+        interescolar_points + Number((participant.resultados as string[])[3]),
+    };
+  });
+  const criteria = data?.criterio_habilitacion
+    ? (data.criterio_habilitacion as number[])
+    : [5, 5, 5];
+  provincialParticipants = provincialParticipants.filter(
+    (participant) => participant.puntos >= criteria[participant.nivel - 1]
+  );
+  const disabled = await getDisabled(competition, year, "PROVINCIAL");
+  provincialParticipants = provincialParticipants.filter(
+    (participant) =>
+      !disabled.some(
+        (disabled_participant) =>
+          disabled_participant.id_participacion === participant.id_participacion
+      )
+  );
+  return provincialParticipants;
+};
+
+export const getResults = async (
+  competition: string,
+  year: number,
+  instance: INSTANCIA
+) => {
+  const results = await prisma.rinde.findMany({
+    where: {
+      prueba: {
+        AND: [
+          { instancia: instance },
+          {
+            competencia: {
+              AND: [{ ano: year }, { tipo: competition }],
+            },
+          },
+        ],
+      },
+    },
+    orderBy: [
+      {
+        participacion: {
+          nivel: "asc",
+        },
+      },
+      {
+        participacion: {
+          participante: {
+            apellido: "asc",
+          },
+        },
+      },
+    ],
+    select: {
+      prueba: {
+        select: {
+          cantidad_problemas: true,
+        },
+      },
+      id_rinde: true,
+      presente: true,
+      aprobado: true,
+      aclaracion: true,
+      resultados: true,
+      participacion: {
+        select: {
+          id_participacion: true,
+        },
+      },
+    },
+  });
+  return results;
+};
+
+export const modifyResult = async (
+  id_rinde: number,
+  score: string[],
+  approved: boolean,
+  present: boolean,
+  clarification: string | null
+) => {
+  const result = await prisma.rinde.update({
+    where: {
+      id_rinde: id_rinde,
+    },
+    data: {
+      resultados: score,
+      aprobado: approved,
+      presente: present,
+      aclaracion: clarification,
+    },
+  });
+  return result;
+};
+
+export const newResult = async (
+  id_participation: number,
+  id_test: number,
+  score: string[],
+  approved: boolean,
+  present: boolean,
+  clarification: string | null
+) => {
+  const result = await prisma.rinde.create({
+    data: {
+      id_participacion: id_participation,
+      id_prueba: id_test,
+      resultados: score,
+      aprobado: approved,
+      presente: present,
+      aclaracion: clarification,
+    },
+  });
+  return result;
+};
 
 export const deleteResult = async (id_rinde: number) => {
-    const result = await prisma.rinde.delete({
-        where: {
-            id_rinde: id_rinde
-        }
-    });
-    return result;
+  const result = await prisma.rinde.delete({
+    where: {
+      id_rinde: id_rinde,
+    },
+  });
+  return result;
+};
+
+export const updateResults = async (
+  results: {
+    id_rinde: number;
+    resultados: string[];
+    aprobado: boolean;
+    presente: boolean;
+    aclaracion: string | null;
+  }[]
+) => {
+  prisma.rinde.updateMany({
+    where: {
+      id_rinde: {
+        in: results.map((result) => result.id_rinde),
+      },
+    },
+    data: results,
+  });
+};
+
+export const createResults = async (
+  results: {
+    dni: number;
+    resultados: string[];
+    aprobado: boolean;
+    presente: boolean;
+    aclaracion: string | null;
+  }[],
+  id_prueba: number
+) => {
+  const ano = (
+    await prisma.prueba.findFirst({
+      where: {
+        id_prueba: id_prueba,
+      },
+      select: {
+        competencia: {
+          select: {
+            ano: true,
+          },
+        },
+      },
+    })
+  )?.competencia.ano as number;
+  const ids_participaciones = await prisma.participacion.findMany({
+    where: {
+      participante: {
+        dni: {
+          in: results.map((result) => result.dni),
+        },
+      },
+      competencia: {
+        ano: ano,
+      },
+    },
+    select: {
+      participante: {
+        select: {
+          dni: true,
+        },
+      },
+      id_participacion: true,
+    },
+  });
+  prisma.rinde.createMany({
+    data: results.map((result) => {
+      return {
+        ...result,
+        id_prueba: id_prueba,
+        id_participacion: ids_participaciones.find(
+          (participation) => participation.participante.dni === result.dni
+        )?.id_participacion as number,
+      };
+    }),
+  });
 };
 
 export type Participant = Awaited<ReturnType<typeof getParticipants>>[0];
