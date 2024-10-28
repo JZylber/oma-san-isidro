f1b2670bf95d5e489ba9b8366a1533b4b8e03df4
JZylber <zylberjulian@gmail.com>
2024-10-28 01:37:43 -0300

    feat(dashboard): home link

--- components/Dashboard/NavBar/NavBar.tsx
+++ components/Dashboard/NavBar/NavBar.tsx
@@ -3,11 +3,19 @@
 import { usePathname } from "next/navigation";
 import NavBarLink from "./NavBarLink";
 import UserChip from "./UserChip";
+import Link from "next/link";
 
 const DashboardNavBar = () => {
   const pathname = usePathname();
   return (
     <nav className="w-[24rem] shrink-0 flex flex-col pl-4 py-4 gap-y-4 bg-primary-light-blue border-r-2 border-primary-black">
+      <div className="flex justify-center items-center py-2 mr-4">
+        <Link href="/" className="flex w-full">
+          <span className="w-full py-8 text-6xl font-unbounded text-primary-black text-center bg-primary-white border-2 border-primary-black rounded-lg">
+            OMA SI
+          </span>
+        </Link>
+      </div>
       <NavBarLink href="/dashboard" selected={pathname === "/dashboard"}>
         Inicio
       </NavBarLink>
