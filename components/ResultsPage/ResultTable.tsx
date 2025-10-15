c0501f561d9611ff322be14e381d1e444445f80f
JZylber <zylberjulian@gmail.com>
2025-10-15 16:07:05 -0300

    chore(results): FINALLY removed revisions

--- components/ResultsPage/ResultTable.tsx
+++ components/ResultsPage/ResultTable.tsx
@@ -153,7 +153,7 @@ const ResultTable = ({
         )}
       {instancia === "REGIONAL" &&
         competencia === "OMA" &&
-        testInfo.año === 2025 && (
+        testInfo.año === 2024 && (
           <Warning title="Pedidos de Revisión" className={styles.revision}>
             <p className={styles.text}>
               Para hacer un pedido de revisión completar el siguiente formulario{" "}
