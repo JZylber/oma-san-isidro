18b7d01142cd81adc14362abfc757c0a9044b3f5
JZylber <zylberjulian@gmail.com>
2026-04-11 20:08:25 -0300

    feat(geometry): 2026

--- app/(public)/otros/geometria/geometria.tsx
+++ app/(public)/otros/geometria/geometria.tsx
@@ -1,23 +1,114 @@
-'use client'
-import { CalendarEvent} from "../../../../components/CalendarComponents/CalendarTypes";
+"use client";
+import { CalendarEvent } from "../../../../components/CalendarComponents/CalendarTypes";
 import styles from "./Geometria.module.scss";
 import BankInformation from "../../../../components/Inscription/BankInformation";
 import AllDatesBanner from "../../../../components/CalendarComponents/DateBanner/NextDatesBanner";
 import Warning from "../../../../components/Warning/Warning";
 import Pending from "../../../../components/Pending/pending";
 
-interface GeometryPageProps{
-    events : Array<CalendarEvent>,
-    year : number
+interface GeometryPageProps {
+  events: Array<CalendarEvent>;
+  year: number;
 }
 
-const GeometryPage = ({events}: GeometryPageProps ) => {
-    return(
-        <>
-                <h1 className={styles.title}>Torneo de Geometría e Imaginación</h1>
-                <AllDatesBanner dates={events} category="Geometría"/>
-                <Pending text="Todavía no hay información sobre la inscripción al torneo de geometría"/>
-                {/*<section className={styles.section}>
+const GeometryPage = ({ events }: GeometryPageProps) => {
+  return (
+    <>
+      <h1 className={styles.title}>Torneo de Geometría e Imaginación</h1>
+      <AllDatesBanner dates={events} category="Geometría" />
+      <section className={styles.section}>
+        <h2 className={styles.section_title}>
+          Festival de Problemas: Primera Ronda TGI
+        </h2>
+        <div className={styles.section_content}>
+          <p>
+            El próximo <span className={styles.bold}>sábado 30 de mayo</span>,
+            se celebra la primera instancia del Torneo de Geometría e
+            Imaginación (TGI). Este evento está diseñado como un espacio de
+            exploración y creatividad, donde el uso de herramientas como
+            GeoGebra y la visualización geométrica son los protagonistas.
+          </p>
+          <p>
+            Es importante destacar que los alumnos pueden participar en las
+            distintas rondas de este torneo de forma{" "}
+            <span className={styles.bold}>independiente</span>. No es requisito
+            haber participado en instancias previas ni existe la obligación de
+            continuar en las siguientes; cada encuentro es una oportunidad única
+            para disfrutar de la matemática.
+          </p>
+          <ul>
+            <li>
+              <span className={[styles.level, styles.bold].join(" ")}>
+                Nivel:
+              </span>{" "}
+              Los participantes podrán elegir libremente los problemas que
+              deseen desarrollar, más allá del nivel sugerido.
+            </li>
+            <li>
+              <span className={[styles.level, styles.bold].join(" ")}>
+                Ambiente:
+              </span>{" "}
+              El festival busca fomentar la sociabilización entre alumnos y
+              docentes, cerrando la jornada con juegos matemáticos y actividades
+              lúdicas.
+            </li>
+            <li>
+              <span className={[styles.level, styles.bold].join(" ")}>
+                Dinámica:
+              </span>{" "}
+              Se trabajará durante todo el día en dos bloques (mañana y tarde)
+              con un espacio para el almuerzo y la discusión de soluciones.
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <h3 className={styles.section_title}>Sede y Logística</h3>
+        <div className={styles.section_content}>
+          <p>
+            Para la región de{" "}
+            <span className={styles.bold}>
+              AMBA y Provincia de Buenos Aires
+            </span>
+            , el festival se centralizará en la siguiente sede:
+          </p>
+          <ul>
+            <li>
+              <span className={[styles.level, styles.bold].join(" ")}>
+                Lugar:
+              </span>{" "}
+              Colegio Belgrano, Localidad de Lomas de Zamora (incluye a las
+              zonas de La Plata y Pilar).
+            </li>
+            <li>
+              <span className={[styles.level, styles.bold].join(" ")}>
+                Costo:
+              </span>{" "}
+              Los participantes de las rondas de TGI tienen cubierta la
+              inscripción a las dos primeras instancias con el pago de los
+              certámenes de OMA/Ñandú. Los secretarios responsables coordinarán
+              localmente el costo de las colaciones y el almuerzo.
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <h3 className={styles.section_title}>Inscripción</h3>
+        <div className={styles.section_content}>
+          <p>
+            Contactarse vía correo electrónico con la organizadora regional{" "}
+            <span className={styles.bold}>Claudia Iravedra</span> a la
+            dirección:
+            <a href="mailto:omaclaudia@hotmail.com">omaclaudia@hotmail.com</a>.
+          </p>
+          <p>
+            ¡Recuerden que el costo está cubierto por la inscripción de
+            OMA/Ñandú!
+          </p>
+        </div>
+      </section>
+
+      {/*<section className={styles.section}>
                     <h3 className={styles.section_title}>Inscripción</h3>
                     <div className={styles.section_content}>
                         <ol>
@@ -58,8 +149,8 @@ const GeometryPage = ({events}: GeometryPageProps ) => {
                         <p>Para más información, escribir a <a href="mailto:roxana.magistrali@gmail.com">roxana.magistrali@gmail.com</a></p>
                     </div>
                 </section>*/}
-        </>  
-       )
-}
+    </>
+  );
+};
 
-export default GeometryPage
+export default GeometryPage;
